1e3c3b2bc8a8bb3bca57da25def238902895e82e
Andreas Lengkeek <andreas.lengkeek@gmail.com>
2019-09-16 17:15:36 +1000

    Convert react native app to regular app

--- ReactNative/index.js
+++ ReactNative/index.js
@@ -1,5 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('ReactNative', () => App);
