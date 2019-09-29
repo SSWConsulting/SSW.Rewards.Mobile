90d1abc7e09c26f96476a7004992af91af474200
William Liebenberg <awliebenberg@outlook.com>
2019-09-30 01:17:44 +1000

    add DI for all the secret providers add DI for all the settings and secret interfaces nested the GetLeaderboardListQuery Handler

--- SSW.Consulting/Startup.cs
+++ SSW.Consulting/Startup.cs
@@ -1,45 +1,85 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using MediatR;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureADB2C.UI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using SSW.Consulting.Application.Interfaces;
 using SSW.Consulting.Application.Leaderboard.Queries.GetLeaderboardList;
-using MediatR;
+using SSW.Consulting.Infrastructure;
+using SSW.Consulting.Persistence;
+using SSW.Consulting.WebAPI.Settings;
+using System;
 using System.Reflection;
 
 namespace SSW.Consulting
 {
 	public class Startup
 	{
-		public Startup(IConfiguration configuration)
+		public Startup(IWebHostEnvironment environment, IConfiguration configuration)
 		{
+			Environment = environment;
 			Configuration = configuration;
 		}
 
+		public IWebHostEnvironment Environment { get; }
 		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-            services.AddAuthentication(AzureADB2CDefaults.BearerAuthenticationScheme)
-                .AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options));
-            services.AddControllers();
+			services.AddAuthentication(AzureADB2CDefaults.BearerAuthenticationScheme)
+				.AddAzureADB2CBearer(options => Configuration.Bind("AzureAdB2C", options));
 
-            // Add MediatR
-            services.AddMediatR(typeof(GetLeaderboardListQueryHandler).GetTypeInfo().Assembly);
-        }
+			services.AddControllers();
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+			// Add MediatR
+			services.AddMediatR(typeof(GetLeaderboardListQuery).GetTypeInfo().Assembly);
+
+			// Configure all the stuffs
+			ConfigureSecretsProviders(services);
+			ConfigureSettings(services);
+			ConfigureSecrets(services);
+
+			// Add SSW Consulting DbContext
+			services.AddScoped<ISSWConsultingDbContent, SSWConsultingDbContent>();
+		}
+
+		protected virtual void ConfigureSecrets(IServiceCollection services)
+		{
+			// Add Secrets
+			// TODO: Perhaps add some registration via convention for anything that implements a nested ISecrets interface
+			services.AddSingleton<SSWConsultingDbContent.ISecrets, SSWConsultingDbContextSecrets>();
+		}
+
+		protected virtual void ConfigureSettings(IServiceCollection services)
+		{
+			// Add Settings
+			// TODO: Perhaps add some registration via convention for anything that implements a nested ISettings interface
+			services.AddSingleton<KeyVaultSecretsProvider.ISettings, AppSettings>();
+		}
+
+		protected virtual void ConfigureSecretsProviders(IServiceCollection services)
+		{
+			// Add Secrets Provider
+#if DEBUG
+			bool useLocalSecrets = Convert.ToBoolean(Configuration["UseLocalSecrets"]);
+			if (useLocalSecrets)
+			{
+				services.AddSingleton<ISecretsProvider, LocalSecretsProvider>();
+			}
+			else
+#endif
+			{
+				services.AddSingleton<ISecretsProvider, KeyVaultSecretsProvider>();
+				services.AddSingleton<IKeyVaultClientProvider, KeyVaultClientProvider>();
+			}
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
 			if (env.IsDevelopment())
 			{
@@ -50,10 +90,10 @@ namespace SSW.Consulting
 
 			app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+			app.UseAuthentication();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
+			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
 			});
