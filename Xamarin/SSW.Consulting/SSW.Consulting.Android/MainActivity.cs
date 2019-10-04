a41abed09555b91dcad3d6e671160812abc41e88
Matt Goldman <mattgoldman@ssw.com.au>
2019-10-04 11:20:37 +1000

    Added android splash screen

--- Xamarin/SSW.Consulting/SSW.Consulting.Android/MainActivity.cs
+++ Xamarin/SSW.Consulting/SSW.Consulting.Android/MainActivity.cs
@@ -13,7 +13,7 @@ using SSW.Consulting.Services;
 
 namespace SSW.Consulting.Droid
 {
-    [Activity(Label = "SSW.Consulting", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation =ScreenOrientation.Portrait)]
+    [Activity(Label = "SSW.Consulting", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation =ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
