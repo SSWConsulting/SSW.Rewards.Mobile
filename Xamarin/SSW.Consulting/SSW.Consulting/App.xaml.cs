f7d3237af8ea7582f53c389f4b11a973574c37cd
Matt Goldman <mattgoldman@ssw.com.au>
2019-10-05 12:27:06 +1000

    Xaml formatting

--- Xamarin/SSW.Consulting/SSW.Consulting/App.xaml.cs
+++ Xamarin/SSW.Consulting/SSW.Consulting/App.xaml.cs
@@ -40,8 +40,6 @@ namespace SSW.Consulting
 
         protected override void OnStart()
         {
-            AppCenter.LogLevel = LogLevel.Verbose;
-
             AppCenter.Start("android=60b96e0a-c6dd-4320-855f-ed58e44ffd00;" +
 				  "ios=e33283b1-7326-447d-baae-e783ece0789b",
 				  typeof(Auth), typeof(Analytics), typeof(Crashes));
