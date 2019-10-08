5ccd1b171ac888211d8e6779aea9b44f03333da4
MattGoldmanSSW <mattgoldman@ssw.com.au>
2019-10-09 06:36:30 +1100

    Added about page

--- Xamarin/SSW.Consulting/SSW.Consulting/PopupPages/AboutSSW.xaml.cs
+++ Xamarin/SSW.Consulting/SSW.Consulting/PopupPages/AboutSSW.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Rg.Plugins.Popup.Pages;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace SSW.Consulting.PopupPages
@@ -10,6 +11,11 @@ namespace SSW.Consulting.PopupPages
         public AboutSSW()
         {
             InitializeComponent();
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://www.ssw.com.au/ssw/Company/AboutUs.aspx", BrowserLaunchMode.SystemPreferred);
         }
     }
 }
