55b7f5d8f5a3cff2964bd238450d9ecdff3f0999
Matt Goldman <mattgoldman@ssw.com.au>
2019-10-11 11:32:25 +1100

    Added email address as query parameter to tech quiz url

--- Xamarin/SSW.Consulting/SSW.Consulting/PopupPages/TechQuiz.xaml.cs
+++ Xamarin/SSW.Consulting/SSW.Consulting/PopupPages/TechQuiz.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Rg.Plugins.Popup.Pages;
 using Rg.Plugins.Popup.Services;
+using SSW.Consulting.Services;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -9,16 +10,19 @@ namespace SSW.Consulting.PopupPages
 {
     public partial class TechQuiz : PopupPage
     {
+        private IUserService _userService { get; set;
+        }
         public TechQuiz()
         {
             InitializeComponent();
+            _userService = Resolver.Resolve<IUserService>();
         }
 
         private async void FindoutMore_Tapped(object sender, EventArgs e)
         {
-            string url = Constants.ApiBaseUrl + "/api/achievement/TechQuiz";
+            string quizUri = Constants.ApiBaseUrl + "/api/achievement/techquiz?user=" + await _userService.GetMyEmailAsync();
 
-            await Browser.OpenAsync(url, BrowserLaunchMode.External);
+            await Browser.OpenAsync(quizUri, BrowserLaunchMode.External);
         }
 
         public async void Handle_CloseTapped(object sender, EventArgs args)
