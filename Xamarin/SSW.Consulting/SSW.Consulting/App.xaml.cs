a2faf638467579a6346d4cc41773c1b74805d78f
jackkranz <jackkranz@me.com>
2019-10-21 14:09:51 +1100

    Added constraints to the iOS launchscreen Storyboard

--- Xamarin/SSW.Consulting/SSW.Consulting/App.xaml.cs
+++ Xamarin/SSW.Consulting/SSW.Consulting/App.xaml.cs
@@ -68,6 +68,7 @@ namespace SSW.Consulting
                 }
                 catch(Exception e)
                 {
+                    Console.WriteLine(e);
                     //await Current.MainPage.DisplayAlert("Service Unavailable", "Looks like the SSW.Consulting service is not currently available. Please try again later.", "OK");
                 }
             }
