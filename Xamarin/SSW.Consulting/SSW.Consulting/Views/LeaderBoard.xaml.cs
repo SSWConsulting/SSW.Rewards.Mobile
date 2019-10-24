c73bfe872ab54a919a8b66fdbea15cd18ce562cd
jackkranz <37816100+jackkranz@users.noreply.github.com>
2019-10-24 12:41:42 +1100

    Leaderboard search (#36)
    * Basic Search bar added
    
    * Wired up event to textChanged handler not to search press
    
    * dismiss keyboard on scroll
    
    * removed whitespace and fixed formatting
    
    * resolve method formatting issues
    
    * Relative layout for searchbar
    
    * updated keyboardUnfocus to occur on scroll instead of focus change
    
    * resize searchbar
    
    * Sticky Search bar fixed some error cases
    
    * rolled back pull to refresh workaround

--- Xamarin/SSW.Consulting/SSW.Consulting/Views/LeaderBoard.xaml.cs
+++ Xamarin/SSW.Consulting/SSW.Consulting/Views/LeaderBoard.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,13 @@ namespace SSW.Consulting.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LeaderBoard : ContentPage
     {
+        private DateTime lastRefreshed;
+        private DateTime typed;
+        private DateTime dismiss;
+        private int lastItemIn;
+        private int lastItemCount;
+        private bool showBar = true;
+
         public LeaderBoard(LeaderBoardViewModel viewModel)
         {
             InitializeComponent();
@@ -25,12 +32,89 @@ namespace SSW.Consulting.Views
             var vm = Resolver.Resolve<LeaderBoardViewModel>();
             vm.Navigation = Navigation;
             BindingContext = vm;
+            ((LeaderBoardViewModel)this.BindingContext).ScrollToMe = ((obj) =>
+            {
+                leaderList.ScrollTo(obj, ScrollToPosition.MakeVisible, true);
+            });
+
         }
 
         private void Tapped(object sender, EventArgs e)
         {
-            DisplayAlert("Tapped", "","OK");
+            DisplayAlert("Tapped", "", "OK");
         }
+
+        private void TextChanged(SearchBar sender, TextChangedEventArgs e)
+        {
+            typed = DateTime.Now;
+            if (e.NewTextValue == null)
+            {
+                sender.Text = "";
+            }
+        }
+
+        private void SearchButtonPress(object sender, EventArgs e)
+        {
+            UnfocusSearchBar(false);
+        }
+
+        private void UnfocusSearchBar(bool debounce)
+        {
+            var shouldDismiss = debounce ? (DateTime.Now - dismiss).Duration().Milliseconds > 600 : true;
+            if (searchView.IsFocused && shouldDismiss)
+            {
+                searchView.Unfocus();
+                dismiss = DateTime.Now;
+            }
+        }
+
+        private void ToggleSearchBar()
+        {
+            
+                if (showBar)
+                {
+                    searchFrame.FadeTo(0, 100);
+                    searchFrame.TranslateTo(0, -50, 150);
+                    leaderList.Margin = new Thickness(0, 0, 0, 0);
+                    showBar = false;
+                }
+                else
+                {
+                    searchFrame.FadeTo(1, 150);
+                    searchFrame.TranslateTo(0, 0, 100);
+                    leaderList.Margin = new Thickness(0, 60, 0, 0);
+                    showBar = true;
+                }
+            
+        }
+
+        private void ItemAppearing(object sender, ItemVisibilityEventArgs e)
+        {
+            
+            if ((DateTime.Now - typed).Duration().Milliseconds > 200)
+            {
+                if (e.ItemIndex > lastItemIn)
+                {
+                    if(showBar && !leaderList.IsRefreshing){
+                        UnfocusSearchBar(false);
+                        ToggleSearchBar();
+                    }
+                }
+                else
+                {
+                    if (!showBar)
+                    {
+                        UnfocusSearchBar(true);
+                        ToggleSearchBar();
+                    }
+
+
+                }
+                lastItemIn = e.ItemIndex;
+            }
+            
+        }
+
 
         protected override void OnAppearing()
         {
