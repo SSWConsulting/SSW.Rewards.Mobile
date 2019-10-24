ce6079ee0f0fbcc08371a20f116d263f47754df1
Andreas Lengkeek <andreas.lengkeek@gmail.com>
2019-10-24 12:40:31 +1100

    Force quit on back button if on root page

--- Xamarin/SSW.Consulting/SSW.Consulting.Android/MainActivity.cs
+++ Xamarin/SSW.Consulting/SSW.Consulting.Android/MainActivity.cs
@@ -42,17 +42,5 @@ namespace SSW.Consulting.Droid
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-
-        public override void OnBackPressed()
-        {
-			if (Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed))
-			{
-
-			}
-			else
-			{
-				//base.OnBackPressed();
-			}
-        }
-    }
+	}
 }
