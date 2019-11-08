7518483d72cd15d19e6ed0672de79338171da242
Jack Kranz <jackkranz@gmail.com>
2019-11-08 13:05:08 +1100

    Added aure b2c npm package

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import b2cauth from 'react-azure-adb2c';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+b2cauth.initialize({
+    tenant: 'sswconsultingapp.onmicrosoft.com',
+    signInPolicy: 'B2C_1_Admin-Signin',
+    applicationId: '33e787e1-caeb-475b-a6dc-7e014d0a252a',
+    cacheLocation: 'sessionStorage',
+    scopes: ['https://sswconsultingapp.onmicrosoft.com/admin/user_impersonation'],
+    redirectUri: 'http://localhost:3000',
+    postLogoutRedirectUri: window.location.origin,
+  });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+  b2cauth.run(() => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+    serviceWorker.unregister();
+  });
