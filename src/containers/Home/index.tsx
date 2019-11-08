bb800a93f180ca7e17c9e31d3bfac76ff93d1709
Andreas Lengkeek <andreas.lengkeek@gmail.com>
2019-11-08 16:52:27 +1100

    Add validate authority false to b2cauth

--- src/containers/Home/index.tsx
+++ src/containers/Home/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import authentication from 'react-azure-adb2c';
+
 
 const Home = () => {
+
+    const signOut = () => {
+        console.log('auth: ', authentication.getAccessToken());
+        authentication.signOut();
+    }
+
     return (
         <div>
             Home
+            <p>
+                <button onClick={signOut}>Sign Out</button>
+            </p>
         </div>
     )
 }
