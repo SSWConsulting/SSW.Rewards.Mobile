0537a1034e00283af87bb7bf4ae4486a9e87fef6
jackkranz <jackkranz@me.com>
2019-12-04 10:21:19 +1100

    removed blank lines

--- API/SSW.Consulting/Startup.cs
+++ API/SSW.Consulting/Startup.cs
@@ -31,12 +31,9 @@ namespace SSW.Consulting
 
         readonly string AllowSpecificOrigins = "_AllowSpecificOrigins";
 
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
 		{
-
-
             // Configure all the stuffs
             ConfigureSettings(services);
 			ConfigureSecrets(services);
