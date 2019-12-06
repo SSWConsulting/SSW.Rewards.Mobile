4557988eb0f1226356e9be226983926391c08f51
jackkranz <jackkranz@me.com>
2019-12-07 10:25:19 +1100

    Take Photo

--- Xamarin/SSW.Rewards/SSW.Rewards/PopupPages/CameraPage.xaml.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards/PopupPages/CameraPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using Rg.Plugins.Popup.Pages;
 using Rg.Plugins.Popup.Services;
 using SSW.Rewards.ViewModels;
-
+using Xamarin.Forms;
 
 namespace SSW.Rewards.PopupPages
 {
@@ -11,6 +13,29 @@ namespace SSW.Rewards.PopupPages
         public async void Handle_Tapped(object sender, EventArgs e)
         {
             await PopupNavigation.Instance.PopAllAsync();
+        }
+
+        async void Handle_takePhotoClicked(object sender, EventArgs e)
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
+                return;
+            }
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                Directory = "Sample",
+                Name = "test.jpg"
+            });
+            if (file == null)
+                return;
+            await DisplayAlert("File Location", file.Path, "OK");
+            image.Source = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
         }
 
         private CameraPageViewModel _viewModel { get; set; }
