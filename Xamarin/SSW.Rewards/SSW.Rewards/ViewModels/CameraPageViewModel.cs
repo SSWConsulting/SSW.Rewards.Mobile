67b91060cf5ed79a268405272a1788cc51af70af
jackkranz <jackkranz@me.com>
2019-12-07 10:11:21 +1100

    added modal for camera

---
+++ Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/CameraPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Rg.Plugins.Popup.Services;
+using SSW.Rewards.Models;
+using SSW.Rewards.PopupPages;
+using SSW.Rewards.Services;
+using Xamarin.Forms;
+using ZXing;
+
+namespace SSW.Rewards.ViewModels
+{
+    public class CameraPageViewModel : BaseViewModel
+    {
+        
+    }
+}
