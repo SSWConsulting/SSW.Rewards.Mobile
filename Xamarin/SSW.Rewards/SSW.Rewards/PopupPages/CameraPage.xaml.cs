e595d75adc2c8786201883cc1d5e89b53ac60e15
jackkranz <jackkranz@me.com>
2019-12-07 11:32:59 +1100

    Refactored to use MVVM

--- Xamarin/SSW.Rewards/SSW.Rewards/PopupPages/CameraPage.xaml.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards/PopupPages/CameraPage.xaml.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using Plugin.Media;
-using Plugin.Media.Abstractions;
 using Rg.Plugins.Popup.Pages;
 using Rg.Plugins.Popup.Services;
 using SSW.Rewards.ViewModels;
-using Xamarin.Forms;
 
 namespace SSW.Rewards.PopupPages
 {
@@ -15,29 +12,6 @@ namespace SSW.Rewards.PopupPages
             await PopupNavigation.Instance.PopAllAsync();
         }
 
-        async void Handle_takePhotoClicked(object sender, EventArgs e)
-        {
-            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
-            {
-                await DisplayAlert("No Camera", ":( No camera avaialble.", "OK");
-                return;
-            }
-            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
-            {
-                Directory = "Sample",
-                Name = "test.jpg"
-            });
-            if (file == null)
-                return;
-            await DisplayAlert("File Location", file.Path, "OK");
-            image.Source = ImageSource.FromStream(() =>
-            {
-                var stream = file.GetStream();
-                file.Dispose();
-                return stream;
-            });
-        }
-
         private CameraPageViewModel _viewModel { get; set; }
 
         public CameraPage()
@@ -45,6 +19,7 @@ namespace SSW.Rewards.PopupPages
             InitializeComponent();
             _viewModel = Resolver.Resolve<CameraPageViewModel>();
             _viewModel.Navigation = Navigation;
+            _viewModel.page = this;
             BindingContext = _viewModel;
         }
     }
--- Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/CameraPageViewModel.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/CameraPageViewModel.cs
@@ -1,17 +1,57 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿
 using System.Windows.Input;
-using Rg.Plugins.Popup.Services;
-using SSW.Rewards.Models;
-using SSW.Rewards.PopupPages;
-using SSW.Rewards.Services;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
 using Xamarin.Forms;
-using ZXing;
 
 namespace SSW.Rewards.ViewModels
 {
     public class CameraPageViewModel : BaseViewModel
     {
-        
+        public ICommand OnTakePhotoTapped { get; set; }
+        public ImageSource ProfilePicture { get; set; } = ImageSource.FromFile("");
+        public Page page;
+
+        public CameraPageViewModel()
+        {
+      
+            OnTakePhotoTapped = new Command(Handle_takePhotoClicked);
+        }
+
+        public void Handle_takePhotoClicked()
+        {
+            CapturePhoto();
+        }
+
+         private async void CapturePhoto()
+        {
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await page.DisplayAlert("No Camera", "We cannot seem to access the Camera", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                Directory = "Temp",
+                Name = "profile.jpg",
+                PhotoSize = PhotoSize.Small
+                
+            });
+
+            if (file == null)
+                return;
+       
+            var image = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+
+
+            ProfilePicture = image;
+            RaisePropertyChanged("ProfilePicture");
+        }
     }
 }
