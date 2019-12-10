acaa84fba33814732b371e6d35ee71460e5d04a3
Matt Goldman <mattgoldman@ssw.com.au>
2019-12-11 10:57:07 +1100

    Added CreatedUtc field

--- API/SSW.Rewards.Persistence/Migrations/20191210235317_AddedCreatedUtc.Designer.cs
+++ API/SSW.Rewards.Persistence/Migrations/20191210235317_AddedCreatedUtc.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SSW.Rewards.Persistence;
 
 namespace SSW.Rewards.Persistence.Migrations
 {
     [DbContext(typeof(SSWRewardsDbContext))]
-    partial class SSWConsultingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191210235317_AddedCreatedUtc")]
+    partial class AddedCreatedUtc
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
