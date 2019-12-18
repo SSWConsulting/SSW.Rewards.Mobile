d0ca4620154ad72c2db8b7a79691f52657074854
Matt Goldman <55862976+MattGoldmanSSW@users.noreply.github.com>
2019-12-18 11:17:18 +1100

    Admin claims (#82)
    * Added admin test controller
    
    * Removed unneccessary components

---
+++ API/SSW.Rewards/Controllers/AdminTestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SSW.Rewards.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminTestController : ControllerBase
+    {
+        [HttpGet("Admin")]
+        [Authorize(Roles ="admin")]
+        public async Task<ActionResult> Admin()
+        {
+            return Ok("admin only");
+        }
+
+        [HttpGet("NotAdmin")]
+        public async Task<ActionResult> NotAdmin()
+        {
+            //var user = HttpContext.User.Claims;
+            return Ok("non-admin permitted");
+        }
+    }
+}
