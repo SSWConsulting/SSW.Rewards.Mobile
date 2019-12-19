9b8f17b98870084c042f648adea1516c68ec1587
jackkranz <jackkranz@me.com>
2019-12-19 14:58:25 +1100

    Added choose photo option

--- Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/CameraPageViewModel.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/CameraPageViewModel.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
@@ -9,22 +10,47 @@ namespace SSW.Rewards.ViewModels
     public class CameraPageViewModel : BaseViewModel
     {
         public ICommand OnTakePhotoTapped { get; set; }
+        public ICommand OnChoosePhotoTapped { get; set; }
+
         public ImageSource ProfilePicture { get; set; } = ImageSource.FromFile("");
         public Page page;
 
         public CameraPageViewModel()
         {
       
-            OnTakePhotoTapped = new Command(Handle_takePhotoClicked);
+            OnTakePhotoTapped = new Command(Handle_takePhotoTapped);
+            OnChoosePhotoTapped = new Command(Handle_choosePhotoTapped);
+
         }
 
-        public void Handle_takePhotoClicked()
+        public void Handle_takePhotoTapped()
         {
             CapturePhoto();
         }
-
-         private async void CapturePhoto()
+        public void Handle_choosePhotoTapped()
         {
+            ChoosePhoto();
+        }
+
+        private void SetPhoto(MediaFile file)
+        {
+            if (file == null)
+                return;
+
+            var image = ImageSource.FromStream(() =>
+            {
+                var stream = file.GetStream();
+                file.Dispose();
+                return stream;
+            });
+
+            ProfilePicture = image;
+            RaisePropertyChanged("ProfilePicture");
+        }
+
+      
+         private async void CapturePhoto()
+         {
             if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
             {
                 await page.DisplayAlert("No Camera", "We cannot seem to access the Camera", "OK");
@@ -39,19 +65,27 @@ namespace SSW.Rewards.ViewModels
                 DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Front
             });
 
+            SetPhoto(file);
+         }
+
+        private async void ChoosePhoto()
+        {
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await page.DisplayAlert("No Camera", "We cannot seem to access the Camera", "OK");
+                return;
+            }
+
+            var file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
+            {
+                PhotoSize = PhotoSize.Small,
+                SaveMetaData = false
+            }) ;
+
             if (file == null)
                 return;
-       
-            var image = ImageSource.FromStream(() =>
-            {
-                var stream = file.GetStream();
-                file.Dispose();
-                return stream;
-            });
 
-
-            ProfilePicture = image;
-            RaisePropertyChanged("ProfilePicture");
+            SetPhoto(file);
         }
     }
 }
