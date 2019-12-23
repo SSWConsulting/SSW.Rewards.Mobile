7cf2f476304b89f16547da16adc8cebe0d83e67e
jackkranz <jackkranz@me.com>
2019-12-23 15:52:36 +1100

    Added update profile pic url endpoint

--- API/SSW.Rewards/Controllers/UserController.cs
+++ API/SSW.Rewards/Controllers/UserController.cs
@@ -1,8 +1,12 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
 using SSW.Rewards.Application.User.Queries.GetCurrentUser;
 using SSW.Rewards.Application.User.Queries.GetUserAchievements;
 using SSW.Rewards.Application.User.Queries.GetUserRewards;
+using System.Security.Claims;
+using System.Linq;
 
 namespace SSW.Rewards.WebAPI.Controllers
 {
@@ -24,6 +28,19 @@ namespace SSW.Rewards.WebAPI.Controllers
         public async Task<ActionResult<UserRewardsViewModel>> Rewards([FromQuery] int userId)
         {
             return Ok(await Mediator.Send(new GetUserRewardsQuery { UserId = userId }));
+        }
+
+        private static string FindFirstAndReplace(IEnumerable<object> e, string search)
+        {
+            return e.First(c => c.ToString().Contains(search)).ToString().Replace(search, string.Empty);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> ProfilePicture([FromBody] string url)
+        {
+            var claim = "emails: ";
+            var email = HttpContext.User.Claims.First(c => c.ToString().Contains(claim)).ToString().Replace(claim, string.Empty).ToLower();
+            return Ok(await Mediator.Send(new UpdateProfilePictureQuery { Email = email , Url = url}));
         }
     }
 }
