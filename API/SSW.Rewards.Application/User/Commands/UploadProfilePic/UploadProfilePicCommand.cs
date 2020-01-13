025424e166e15b9b42cd38218fa84d273966f8d9
Matt Goldman <mattgoldman@ssw.com.au>
2020-01-13 12:04:28 +1100

    Added uploadpicturecommand, populated validator

---
+++ API/SSW.Rewards.Application/User/Commands/UploadProfilePic/UploadProfilePicCommand.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using SSW.Rewards.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SSW.Rewards.Application.User.Commands.UploadProfilePic
+{
+    public class UploadProfilePicCommand : IRequest<UploadProfilePicResult>
+    {
+        public IFormFile File { get; set; }
+
+        public class UploadProfilePicHandler : IRequestHandler<UploadProfilePicCommand, UploadProfilePicResult>
+        {
+            private readonly IMapper _mapper;
+            private readonly ISSWRewardsDbContext _context;
+            private readonly IProfilePicStorageProvider _storage;
+            public ICurrentUserService _currentUserService { get; }
+
+            public UploadProfilePicHandler(ICurrentUserService currentUserService,
+                ISSWRewardsDbContext sSWRewardsDbContext,
+                IProfilePicStorageProvider profilePicStorageProvider,
+                IMapper mapper)
+            {
+                _currentUserService = currentUserService;
+                _mapper = mapper;
+                _storage = profilePicStorageProvider;
+                _context = sSWRewardsDbContext;
+            }
+
+            public async Task<UploadProfilePicResult> Handle(UploadProfilePicCommand request, CancellationToken cancellationToken)
+            {
+                
+            }
+        }
+    }
+}
---
+++ API/SSW.Rewards.Application/User/Commands/UploadProfilePic/UploadProfilePicResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SSW.Rewards.Application.User.Commands.UploadProfilePic
+{
+    public class UploadProfilePicResult
+    {
+    }
+}
