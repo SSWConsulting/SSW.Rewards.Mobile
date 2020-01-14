00e40c03912d268d770a6ada6163e3e89eea3ba5
Matt Goldman <mattgoldman@ssw.com.au>
2020-01-14 10:47:58 +1000

    Removed unused comments

--- API/SSW.Rewards.Application/User/Queries/GetCurrentUser/AutoMapperProfile.cs
+++ API/SSW.Rewards.Application/User/Queries/GetCurrentUser/AutoMapperProfile.cs
@@ -13,7 +13,7 @@ namespace SSW.Rewards.Application.User.Queries.GetCurrentUser
             {
                 CreateMap<Domain.Entities.User, CurrentUserViewModel>()
                 .ForMember(dst => dst.Points, opt => opt.MapFrom(src => src.UserAchievements.Sum(ua => ua.Achievement.Value)))
-                .ForMember(dst => dst.ProfilePic, opt => opt.MapFrom(src => src.Avatar));// == null ? null : new Uri(src.Avatar)));
+                .ForMember(dst => dst.ProfilePic, opt => opt.MapFrom(src => src.Avatar));
             }
             catch(Exception e)
             {
