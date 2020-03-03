c249084434c85949eb379fd2036e37e94ea4bdaf
jackkranz <37816100+jackkranz@users.noreply.github.com>
2020-03-03 12:25:26 +1100

    Added typesacript client (#105)

---
+++ API/SSW.Rewards/nswag/BaseClient.ts
@@ -0,0 +1,13 @@
+﻿export class BaseClient {
+    public token: string = "";
+    private readonly AUTH_HEADER = "Authorization";
+
+    setAuthToken(token: string): void {
+        this.token = token;
+    }
+
+    transformOptions(options: any): Promise<any> {
+        options.headers[this.AUTH_HEADER] = `Bearer ${this.token}`;
+        return Promise.resolve(options);
+    }
+}
