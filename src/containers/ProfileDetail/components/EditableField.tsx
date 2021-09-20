1e5554705edc35b862d58d6dddcb765db7cbc26e
Jake Bayliss <JakeBayliss@ssw.com.au>
2021-09-20 17:05:18 +1000

    API - Updating fields

--- src/containers/ProfileDetail/components/EditableField.tsx
+++ src/containers/ProfileDetail/components/EditableField.tsx
@@ -3,22 +3,23 @@ import { Typography, TextField } from '@material-ui/core'
 
 interface EditableFieldProps {
     label: string;
+    name?: string;
     value?: string;
     type?: string;
     style?: object;
+    onChange: Function;
 }
 
 const EditableField = (props: EditableFieldProps) => {
     const [value, setValue] = useState(props.value);
     const [type, setType] = useState(props.type);
 
-
     return <div style={{ width: '100%', padding: '10px' }}>
         <Typography variant="body1" style={{margin: '10px 0 3px 5px'}}>{props.label}</Typography>
         {type == 'multiline' ? (
-            <TextField value={value} variant="outlined" onChange={(e) => setValue(e.target.value)} style={props.style} multiline />
+            <TextField value={value} variant="outlined" onChange={(e) => props.onChange(props.name, e.target.value)} style={props.style} multiline />
         ) : (
-            <TextField value={value} variant="outlined" onChange={(e) => setValue(e.target.value)} style={props.style} />
+            <TextField value={value} variant="outlined" onChange={(e) => props.onChange(props.name, e.target.value)} style={props.style} />
         )}
         
     </div>
--- src/containers/ProfileDetail/index.tsx
+++ src/containers/ProfileDetail/index.tsx
@@ -13,43 +13,44 @@ export interface RouteParams {
 }
 
 const ProfileDetailComponent = () => {
-    const [loading, setLoading] = useState(true);
-
     const [state, updateState] = useGlobalState<State>();
     const client: StaffClient = useAuthenticatedClient<StaffClient>(state.staffClient, state.token);
-
     const { name } = useParams<RouteParams>();
 
-    const getProfile = async () => {
-        setLoading(true);
-        const response = await fetchData<StaffDto>(() => client.getStaffMemberProfile(name));
-        setLoading(false);
-        response && updateState({ profileDetail: response });
-    };
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        setLoading(true);
-        client && getProfile();
+        (async () => {
+            const response = await fetchData<StaffDto>(() => client.getStaffMemberProfile(name));
+            setLoading(false);
+            updateState({ profileDetail: response });
+        })();
     }, [client]);
 
-    const saveChanges = () => {
-        return '';
+    const handleValueChange = (label: string, newValue: string) => {
+        var profile = state.profileDetail;
+        (profile as any)[label] = newValue;
+        updateState({ profileDetail: profile });
+    }
+
+    const saveChanges = async () => {
+        await client.upsertStaffMemberProfile(state.profileDetail);
     }
 
     return <div>
         {!loading && state.profileDetail ? (
-            <div style={{display: 'flex'}}>
+            <div style={{ display: 'flex' }}>
                 <img src={state.profileDetail.profilePhoto} style={{width: 247.5, height: 495}}/>
-                <div style={{padding: '10px', width: '100%'}}>
-                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        <EditableField label="Name" value={state.profileDetail.name} style={{ width: '100%' }} />
-                        <EditableField label="Title" value={state.profileDetail.title} style={{ width: '100%' }} />
+                <div style={{ paddingLeft: '10px', width: '99%' }}>
+                    <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+                        <EditableField name="name" label="Name" value={state.profileDetail.name} style={{ width: '100%' }} onChange={handleValueChange} />
+                        <EditableField name="title" label="Title" value={state.profileDetail.title} style={{ width: '100%' }} onChange={handleValueChange} />
                     </div>
                     <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                        <EditableField label="Email" value={state.profileDetail.email} style={{ width: '100%' }} />
-                        <EditableField label="Twitter handle" value={state.profileDetail.twitterUsername} style={{ width: '100%' }} />
+                        <EditableField name="email" label="Email" value={state.profileDetail.email} style={{ width: '100%' }} onChange={handleValueChange} />
+                        <EditableField name="twitterUsername" label="Twitter handle" value={state.profileDetail.twitterUsername} style={{ width: '100%' }} onChange={handleValueChange} />
                     </div>
-                    <EditableField label="Profile" value={state.profileDetail.profile} style={{ width: '100%' }} type="multiline" />
+                    <EditableField name="profile" label="Profile" value={state.profileDetail.profile} style={{ width: '100%' }} type="multiline" onChange={handleValueChange} />
                     <Button variant="contained" style={{ margin: '10px' }} onClick={() => saveChanges()}>Save Changes</Button>
                 </div>
             </div>
