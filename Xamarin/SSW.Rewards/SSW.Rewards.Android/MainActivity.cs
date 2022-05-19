22755db8c88c89bd1edfb017b0c35802c6023571
Anna <jianganna7@hotmail.com>
2022-05-19 14:24:22 +1000

    Configured Android project to work with PN

--- Xamarin/SSW.Rewards/SSW.Rewards.Android/MainActivity.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards.Android/MainActivity.cs
@@ -6,17 +6,44 @@ using Lottie.Forms.Droid;
 using PanCardView.Droid;
 using Plugin.CurrentActivity;
 
+using System;
+using Android.Content;
+using SSW.Rewards.Droid.Services;
+using SSW.Rewards.Services;
+using Firebase.Messaging;
+
 namespace SSW.Rewards.Droid
 {
-    [Activity(Label = "SSW Rewards", Icon = "@mipmap/ic_launcher", RoundIcon = "@mipmap/ic_launcher_round", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation =ScreenOrientation.Portrait)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "SSW Rewards", LaunchMode = LaunchMode.SingleTop, Icon = "@mipmap/ic_launcher", RoundIcon = "@mipmap/ic_launcher_round", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation =ScreenOrientation.Portrait)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, Android.Gms.Tasks.IOnSuccessListener
     {
+        IPushNotificationActionService _notificationActionService;
+        IDeviceInstallationService _deviceInstallationService;
+
+        IPushNotificationActionService NotificationActionService
+            => _notificationActionService ??
+                (_notificationActionService =
+                ServiceContainer.Resolve<IPushNotificationActionService>());
+
+        IDeviceInstallationService DeviceInstallationService
+            => _deviceInstallationService ??
+                (_deviceInstallationService =
+                ServiceContainer.Resolve<IDeviceInstallationService>());
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            Bootstrap.Begin(() => new DeviceInstallationService());
+
+            if (DeviceInstallationService.NotificationsSupported)
+            {
+                FirebaseMessaging.Instance
+                    .GetToken()
+                    .AddOnSuccessListener(this);
+            }
 
             CrossCurrentActivity.Current.Init(this, savedInstanceState);
             ZXing.Net.Mobile.Forms.Android.Platform.Init();
@@ -30,8 +57,36 @@ namespace SSW.Rewards.Droid
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
 
             LoadApplication(new App());
+            ProcessNotificationActions(Intent);
             App.UIParent = this;
         }
+
+        /// <summary>Conditionally trigger action using the <see cref="IPushNotificationActionService"/> implementation.</summary>
+        void ProcessNotificationActions(Intent intent)
+        {
+            try
+            {
+                if (intent?.HasExtra("action") == true)
+                {
+                    var action = intent.GetStringExtra("action");
+
+                    if (!string.IsNullOrEmpty(action))
+                        NotificationActionService.TriggerAction(action);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>Must handle an incoming intent in both OnCreate and OnNewIntent methods as LaunchMode for the Activity is set to SingleTop.</summary>
+        protected override void OnNewIntent(Intent intent)
+        {
+            base.OnNewIntent(intent);
+            ProcessNotificationActions(intent);
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -45,5 +100,22 @@ namespace SSW.Rewards.Droid
             base.OnResume();
             Xamarin.Essentials.Platform.OnResume();
         }
+
+        public void OnSuccess(Java.Lang.Object result)
+        => DeviceInstallationService.Token = result.ToString();
     }
+
+    //public class OnSuccessListner : Java.Lang.Object, IOnSuccessListener
+    //{
+    //    public void OnFailure(Java.Lang.Exception e)
+    //    {
+    //        Console.WriteLine(e.Message);
+    //    }
+
+    //    void IOnSuccessListener.OnSuccess(Java.Lang.Object result)
+    //    {
+    //        Console.WriteLine(result);
+    //    }
+
+    //}
 }
---
+++ Xamarin/SSW.Rewards/SSW.Rewards.Android/Services/DeviceInstallationService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Android.App;
+using Android.Gms.Common;
+using SSW.Rewards.Models;
+using SSW.Rewards.Services;
+using static Android.Provider.Settings;
+
+namespace SSW.Rewards.Droid.Services
+{
+    public class DeviceInstallationService: IDeviceInstallationService
+    {
+        public string Token { get; set; }
+
+        public bool NotificationsSupported
+            => GoogleApiAvailability.Instance
+                .IsGooglePlayServicesAvailable(Application.Context) == ConnectionResult.Success;
+
+        public string GetDeviceId()
+            => Secure.GetString(Application.Context.ContentResolver, Secure.AndroidId);
+
+        public DeviceInstall GetDeviceInstallation(params string[] tags)
+        {
+            if (!NotificationsSupported)
+                throw new Exception(GetPlayServicesError());
+
+            if (string.IsNullOrWhiteSpace(Token))
+                throw new Exception("Unable to resolve token for FCM");
+
+            var installation = new DeviceInstall
+            {
+                InstallationId = GetDeviceId(),
+                Platform = "fcm",
+                PushChannel = Token
+            };
+
+            installation.Tags.AddRange(tags);
+
+            return installation;
+        }
+
+        string GetPlayServicesError()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(Application.Context);
+
+            if (resultCode != ConnectionResult.Success)
+                return GoogleApiAvailability.Instance.IsUserResolvableError(resultCode) ?
+                           GoogleApiAvailability.Instance.GetErrorString(resultCode) :
+                           "This device is not supported";
+
+            return "An error occurred preventing the use of push notifications";
+        }
+    }
+}
