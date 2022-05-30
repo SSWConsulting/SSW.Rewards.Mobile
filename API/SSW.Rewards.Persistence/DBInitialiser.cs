6a9061377c4b00308df03578b480b2795787cf32
Matt Goldman <matt.goldman@outlook.com>
2022-05-30 18:15:58 +1000

    most of the view logic wired up for profile page carousel

--- API/SSW.Rewards.Persistence/DBInitialiser.cs
+++ API/SSW.Rewards.Persistence/DBInitialiser.cs
@@ -77,5 +77,29 @@ namespace SSW.Rewards.Persistence
 
             await _context.SaveChangesAsync();
         }
+
+        public async Task MapAchievementTypes()
+        {
+            var achievements = await _context.Achievements.ToListAsync();
+
+            var staffAchievements = await _context.StaffMembers
+                .Include(s => s.StaffAchievement)
+                .Select(s => s.StaffAchievement)
+                .ToListAsync();
+
+            foreach (var achievement in achievements)
+            {
+                if (staffAchievements.Contains(achievement))
+                {
+                    achievement.Type = AchievementType.Scanned;
+                }
+                else
+                {
+                    achievement.Type = AchievementType.Attended;
+                }
+            }
+
+            await _context.SaveChangesAsync();
+        }
     }
 }
---
+++ API/SSW.Rewards.Persistence/Migrations/20220530071434_AddAchievmentType.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SSW.Rewards.Persistence.Migrations
+{
+    public partial class AddAchievmentType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Achievements",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Achievements");
+        }
+    }
+}
