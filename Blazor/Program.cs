8836b96f689a11516606a562d0401d74aff48d04
Jack Pettit <JackPettit@ssw.com.au>
2022-05-31 10:13:34 +0000

    Merged PR 3951: ✨Adds MudBlazor
    ✨Adds MudBlazor
    ✨Adds MudBlazor Theme (Dark mode and light mode) - Defaults to users computer preference
    ✨Generic table Component
    ✨ QRCode Component
    ✨Add gitignore file
    🔥Remove all unneeded files
    
    ### Scrrenshots
    
    ![image.png](https://dev.azure.com/ssw/dacc617d-2dc5-4f9e-aa12-ece4e4fe66b7/_apis/git/repositories/3d9abe5e-2dd9-479e-87b0-23b3803821c2/pullRequests/3951/attachments/image.png)
    **Figure: HomePage**
    ![image (2).png](https://dev.azure.com/ssw/dacc617d-2dc5-4f9e-aa12-ece4e4fe66b7/_apis/git/repositories/3d9abe5e-2dd9-479e-87b0-23b3803821c2/pullRequests/3951/attachments/image%20%282%29.png)
    **Figure: Achievements Page with QRCode working**
    ![image (3).png](https://dev.azure.com/ssw/dacc617d-2dc5-4f9e-aa12-ece4e4fe66b7/_apis/git/repositories/3d9abe5e-2dd9-479e-87b0-23b3803821c2/pullRequests/3951/attachments/image%20%283%29.png)
    **Figure: Profiles Page**
    ![image (4).png](https://dev.azure.com/ssw/dacc617d-2dc5-4f9e-aa12-ece4e4fe66b7/_apis/git/repositories/3d9abe5e-2dd9-479e-87b0-23b3803821c2/pullRequests/3951/attachments/image%20%284%29.png)
    **Figure: Leaderboard page - Table with no data**

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Net.Http;
 using SSW.Rewards.Admin;
+using MudBlazor.Services;
 using SSW.Rewards.Admin.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -17,6 +16,7 @@ if (apiBaseUrl == null)
 {
     throw new NullReferenceException("No API base URL provided");
 }
+
 // Http Client
 builder.Services.AddHttpClient(Constants.RewardsApiClient, client => client.BaseAddress = new Uri(apiBaseUrl))
     .AddHttpMessageHandler(sp => sp.GetRequiredService<CustomAuthorizationMessageHandler>());
@@ -26,7 +26,7 @@ builder.Services.AddScoped<AchievementsService>();
 builder.Services.AddScoped<LeaderboardService>();
 builder.Services.AddScoped<RewardsService>();
 builder.Services.AddScoped<StaffService>();
-
+builder.Services.AddMudServices();
 
 string[] authScopes = builder.Configuration.GetSection("Local:Scopes").Get<string[]>();
 
