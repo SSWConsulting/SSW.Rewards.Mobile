1a464cf3e41a0a83f194f7046361f3143367ebea
Matt Goldman <matt.goldman@outlook.com>
2022-06-01 19:01:17 +1000

    people v2 complete

---
+++ API/SSW.Rewards.Persistence/Migrations/20220601072142_AddStaffSkillLevel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SSW.Rewards.Persistence.Migrations
+{
+    public partial class AddStaffSkillLevel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Level",
+                table: "StaffMemberSkills",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "StaffMemberSkills");
+        }
+    }
+}
