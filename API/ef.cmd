73a67002b0a34865e61695e0b1b3d33cf295f71f
JackDevAU <jpr178@live.com.au>
2022-06-08 16:01:24 +1000

    ✏️ Uncomment and change these files back

--- API/ef.cmd
+++ API/ef.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-set configProject=C:\Users\jpr17\src\Internal\SSW.Rewards\API\SSW.Rewards\SSW.Rewards.WebAPI.csproj
-set dbProject=C:\Users\jpr17\src\Internal\SSW.Rewards\API\SSW.Rewards.Persistence\SSW.Rewards.Persistence.csproj
+set configProject=SSW.Consulting\SSW.Consulting.WebAPI.csproj
+set dbProject=SSW.Consulting.Persistence\SSW.Consulting.Persistence.csproj
 
 dotnet ef %* --startup-project "%configProject%" --project "%dbProject%"
 
