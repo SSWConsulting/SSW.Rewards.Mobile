8437efb82a36bf03bd3f6179da190956ad8177aa
Matt Goldman <matt.goldman@outlook.com>
2022-06-17 18:00:39 +1000

    formatting

--- Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/DevProfilesViewModel.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/DevProfilesViewModel.cs
@@ -105,8 +105,8 @@ namespace SSW.Rewards.ViewModels
 
                     _lastProfileIndex = Profiles.Count - 1;
 
-                //SelectedProfile = Profiles[_lastProfileIndex];
-                //OnPropertyChanged(nameof(SelectedProfile));
+                    //SelectedProfile = Profiles[_lastProfileIndex];
+                    //OnPropertyChanged(nameof(SelectedProfile));
 
                     ShowDevCards = true;
                     OnPropertyChanged(nameof(ShowDevCards));
