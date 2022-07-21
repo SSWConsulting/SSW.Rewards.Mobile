791f79845327e18c960b2f784af5d109a30164d2
Matt Goldman <matt.goldman@outlook.com>
2022-07-21 11:42:36 +1000

    Started fleshing out quiz page

---
+++ Xamarin/SSW.Rewards/SSW.Rewards/Converters/PassedToBackgroundConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace SSW.Rewards.Converters
+{
+    public class PassedToBackgroundConverter : IValueConverter
+    {
+        public Color Passed
+        {
+            get
+            {
+                return (Color)Application.Current.Resources["PassedQuiz"];
+            }
+        }
+
+        public Color NotPassed
+        {
+            get
+            {
+                return (Color)Application.Current.Resources["Quiz"];
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((bool)value)
+            {
+                return Passed;
+            }
+            else
+            {
+                return NotPassed;
+            }
+                    
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Xamarin/SSW.Rewards/SSW.Rewards/Helpers/Icon.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards/Helpers/Icon.cs
@@ -23,6 +23,8 @@
 
         public const string Facebook = "\uf09a";
 
+        public const string Angular = "\uf420";
+
 
         // Unbranded icons
 
@@ -77,5 +79,7 @@
         public const string Infor = "\uf4a3";
 
         public const string QRCode = "\uf635";
+
+        public const string StarburstCheck = "\ue30b";
     }
 }
