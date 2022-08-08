a0a48cc465ffb39cba13e65ddd179e904d85f78c
Matt Goldman <matt.goldman@outlook.com>
2022-08-08 16:56:15 +1000

    fixed up assets

--- Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/OnBoardingViewModel.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards/ViewModels/OnBoardingViewModel.cs
@@ -61,9 +61,9 @@ namespace SSW.Rewards.ViewModels
                 },
                 new CarouselViewModel
                 {
-                    Content = "Earn enough points and you could claim a smart water bottle with touch activated content thermometer.",
+                    Content = "Earn enough points and you could claim a cool SSW Keepcup.",
                     Image = "v2cups",
-                    SubHeading = "SSW Smart Keepcup",
+                    SubHeading = "SSW Keepcup",
                     HasPoints = true,
                     Points = 2000,
                     ButtonText = "NEXT",
