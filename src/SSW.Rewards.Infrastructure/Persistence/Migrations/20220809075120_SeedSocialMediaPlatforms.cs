196181af90c0a6fb760b09ab26a0a96e1a4e9a10
Luke Cook <LukeCook@ssw.com.au>
2022-09-02 10:18:06 +1000

    fixed sql syntax

--- src/SSW.Rewards.Infrastructure/Persistence/Migrations/20220809075120_SeedSocialMediaPlatforms.cs
+++ src/SSW.Rewards.Infrastructure/Persistence/Migrations/20220809075120_SeedSocialMediaPlatforms.cs
@@ -8,22 +8,22 @@ namespace SSW.Rewards.Persistence.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            UpsertSocialMediaPlatforms(migrationBuilder, "GitHub");
-            UpsertSocialMediaPlatforms(migrationBuilder, "LinkedIn");
-            UpsertSocialMediaPlatforms(migrationBuilder, "Twitter");
+            UpsertSocialMediaPlatforms(migrationBuilder, "GitHub", "Follow SSW on");
+            UpsertSocialMediaPlatforms(migrationBuilder, "LinkedIn", "Follow SSW on");
+            UpsertSocialMediaPlatforms(migrationBuilder, "Twitter", "Follow SSW TV on");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
 
         }
-        private void UpsertSocialMediaPlatforms(MigrationBuilder migrationBuilder, string platformName)
+        private void UpsertSocialMediaPlatforms(MigrationBuilder migrationBuilder, string platformName, string achievementNameString)
         {
             migrationBuilder.Sql(@$"
                 IF NOT EXISTS (SELECT 1 FROM dbo.SocialMediaPlatforms WHERE Name = '{platformName}')
                 BEGIN
                     INSERT INTO dbo.SocialMediaPlatforms (Name, CreatedUtc, AchievementId) 
-                    values ('{platformName}', GETDATE(), SELECT Id FROM Achievements WHERE Name = 'Follow SSW on {platformName}')
+                    values ('{platformName}', GETDATE(), (SELECT Id FROM Achievements WHERE Name = '{achievementNameString} {platformName}'))
                 END
             ");
         }
