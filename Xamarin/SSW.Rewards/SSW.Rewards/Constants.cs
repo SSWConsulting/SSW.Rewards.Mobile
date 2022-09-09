e1444b548f57fc38e803ef37540dd38865f77307
Matt Goldman <matt.goldman@outlook.com>
2022-09-09 14:33:09 +1000

    removed debug url

--- Xamarin/SSW.Rewards/SSW.Rewards/Constants.cs
+++ Xamarin/SSW.Rewards/SSW.Rewards/Constants.cs
@@ -3,7 +3,7 @@
     public class Constants
     {
 #if DEBUG
-        public string ApiBaseUrl = "https://rwds.goforgoldman.com";//"https://sswconsulting-dev.azurewebsites.net";
+        public string ApiBaseUrl = "https://sswconsulting-dev.azurewebsites.net";
         public string AppCenterAndroidId = "bfe53aa1-a7df-499d-900f-725a5222fc23";
 
 #elif QA
