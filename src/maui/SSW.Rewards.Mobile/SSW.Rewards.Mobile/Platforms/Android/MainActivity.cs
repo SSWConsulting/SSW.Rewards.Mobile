83aa55d28d154c93c5c16c408fcf90942cee9813
Anton Polkanov <ap.polkanov@gmail.com>
2023-02-07 17:30:36 +1100

    fix status bar, navigation bar and splash screen

--- src/maui/SSW.Rewards.Mobile/SSW.Rewards.Mobile/Platforms/Android/MainActivity.cs
+++ src/maui/SSW.Rewards.Mobile/SSW.Rewards.Mobile/Platforms/Android/MainActivity.cs
@@ -1,10 +1,18 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+using Color = Microsoft.Maui.Graphics.Color;
 
 namespace SSW.Rewards.Mobile;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        var MainBackground = Color.FromArgb("#FF121212").ToAndroid();
+        Window!.SetNavigationBarColor(MainBackground);
+        base.OnCreate(savedInstanceState);
+    }
 }
