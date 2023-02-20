017a0cb261c16870ef2dbf0d68b2cd560c757c96
Anton Polkanov <AntonPolkanov@ssw.com.au>
2023-02-21 10:38:13 +1100

    portrait orientation only

--- src/maui/SSW.Rewards.Mobile/SSW.Rewards.Mobile/Platforms/Android/MainActivity.cs
+++ src/maui/SSW.Rewards.Mobile/SSW.Rewards.Mobile/Platforms/Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Color = Microsoft.Maui.Graphics.Color;
 
 namespace SSW.Rewards.Mobile;
 
-[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Portrait)]
 public class MainActivity : MauiAppCompatActivity
 {
     protected override void OnCreate(Bundle savedInstanceState)
