c8f3acbb8d2f768e05307aadca2dd623a73e933a
Anton Polkanov <AntonPolkanov@ssw.com.au>
2023-02-22 13:43:26 +1100

    add converter to SkLottieImageSource

---
+++ src/maui/SSW.Rewards.Mobile/SSW.Rewards.Mobile/Converters/StringToSkLottieImageSourceConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using Microsoft.IdentityModel.Tokens;
+using SkiaSharp.Extended.UI.Controls.Converters;
+
+namespace SSW.Rewards.Mobile.Converters
+{
+    class StringToSkLottieImageSourceConverter : IValueConverter, IMarkupExtension
+    {
+        private readonly SKLottieImageSourceConverter _lottieConverter = new();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var file = (string)value;
+            return file.IsNullOrEmpty() ? null : _lottieConverter.ConvertFromString(file);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
