6dc225a164a914770e5aaa1f432da76941948f71
Anton Polkanov <ap.polkanov@gmail.com>
2023-07-19 12:21:15 +1000

    upgrade to .NET 8.0.100-preview.6.23330.14

--- src/maui/SSW.Rewards.Mobile/SSW.Rewards.Mobile/Platforms/iOS/Program.cs
+++ src/maui/SSW.Rewards.Mobile/SSW.Rewards.Mobile/Platforms/iOS/Program.cs
@@ -1,14 +1,23 @@
-﻿using UIKit;
+﻿using IdentityModel.Client;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using ObjCRuntime;
+using System;
+using System.Runtime.Serialization;
+using System.Text;
+using UIKit;
 
 namespace SSW.Rewards.Mobile;
 
 public class Program
 {
-	// This is the main entry point of the application.
-	static void Main(string[] args)
-	{
-		// if you want to use a different Application Delegate class from "AppDelegate"
-		// you can specify it here.
+    // This is the main entry point of the application.
+    static void Main(string[] args)
+    {
+        // if you want to use a different Application Delegate class from "AppDelegate"
+        // you can specify it here.
 		UIApplication.Main(args, null, typeof(AppDelegate));
 	}
 }
