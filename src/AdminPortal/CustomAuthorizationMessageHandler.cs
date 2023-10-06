c0a2a9bff1b0dcc7c09ec9f7d0422169f2112446
Matt Goldman <matt.goldman@outlook.com>
2023-10-06 11:50:48 +1100

    Renamed to Mobile, added Admin Portal

---
+++ src/AdminPortal/CustomAuthorizationMessageHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace SSW.Rewards.Admin.UI;
+public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
+{
+    private readonly IConfiguration _configuration;
+
+    public CustomAuthorizationMessageHandler(
+        IAccessTokenProvider provider,
+        IConfiguration config,
+        NavigationManager navigationManager)
+        : base(provider, navigationManager)
+    {
+        _configuration = config;
+        ConfigureHandler(
+            authorizedUrls: new[] { _configuration.GetValue<string>("RewardsApiUrl") },
+            scopes: new[] { "email", "profile", "ssw-rewards-api" });
+    }
+}
---
+++ src/AdminPortal/Program.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using MudBlazor;
+using MudBlazor.Services;
+
+namespace SSW.Rewards.Admin.UI;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        // MessageHandler for adding the JWT to outbound requests to the API
+        builder.Services.AddTransient<CustomAuthorizationMessageHandler>();
+
+        string? identityUrl = builder.Configuration["Local:Authority"];
+        if (identityUrl == null)
+        {
+            throw new NullReferenceException("No Authority URL provided");
+        }
+
+        const string identityClientName = "IdentityClient";
+        builder.Services.AddHttpClient(
+            identityClientName,
+            o =>
+            {
+                o.BaseAddress = new Uri(identityUrl);
+                o.DefaultRequestHeaders.Add("Accept", "application/json");
+            })
+            .AddHttpMessageHandler(sp => sp.GetRequiredService<CustomAuthorizationMessageHandler>());
+
+        string? apiBaseUrl = builder.Configuration["RewardsApiUrl"];
+        if (apiBaseUrl == null)
+        {
+            throw new NullReferenceException("No API base URL provided");
+        }
+
+        // register services from nswag
+        const string generatedClientName = "generatedClient";
+        builder.Services.AddHttpClient(generatedClientName)
+            .AddHttpMessageHandler(sp => sp.GetRequiredService<CustomAuthorizationMessageHandler>());
+
+        var generatedClients = typeof(Program).Assembly
+            .GetTypes()
+            .Select(s => new
+            {
+                Implementation = s,
+                Interface = s.GetInterface($"I{s.Name}"), // nswag follows this convention
+            })
+            .Where(x => x.Interface != null);
+
+        foreach (var client in generatedClients)
+        {
+            builder.Services.AddScoped(client.Interface!, sp =>
+            {
+                var ctor = client.Implementation.GetConstructor(new[] { typeof(string), typeof(HttpClient) }) !;
+                return ctor.Invoke(new object?[] { apiBaseUrl, sp.GetService<IHttpClientFactory>() !.CreateClient(generatedClientName) });
+            });
+        }
+
+        builder.Services.AddMudServices(config =>
+        {
+            config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+            config.SnackbarConfiguration.PreventDuplicates = true;
+            config.SnackbarConfiguration.NewestOnTop = false;
+            config.SnackbarConfiguration.ShowCloseIcon = true;
+            config.SnackbarConfiguration.VisibleStateDuration = 2000;
+            config.SnackbarConfiguration.HideTransitionDuration = 500;
+            config.SnackbarConfiguration.ShowTransitionDuration = 500;
+            config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+        });
+
+        string[] authScopes = builder.Configuration.GetSection("Local:Scopes").Get<string[]>();
+
+        builder.Services.AddOidcAuthentication(options =>
+        {
+            builder.Configuration.Bind("Local", options.ProviderOptions);
+
+            foreach (var scope in authScopes)
+            {
+                options.ProviderOptions.DefaultScopes.Add(scope);
+            }
+
+            options.ProviderOptions.ResponseType = "code";
+        });
+
+        await builder.Build().RunAsync();
+    }
+}
