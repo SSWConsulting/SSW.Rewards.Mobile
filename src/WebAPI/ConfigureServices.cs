ca2a5c315be2eab0ecdb0d7b6e794c375eed23be
Matt Goldman <matt.goldman@outlook.com>
2023-11-28 17:59:58 +1100

    Upgraded to .NET 8

---
+++ src/WebAPI/ConfigureServices.cs
@@ -0,0 +1,83 @@
+﻿using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using SSW.Rewards.Application.Common.Interfaces;
+using SSW.Rewards.Infrastructure.Persistence;
+using SSW.Rewards.WebUI.Filters;
+using SSW.Rewards.WebUI.Services;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddWebUIServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+        services.AddHttpContextAccessor();
+
+        services.AddHealthChecks()
+            .AddDbContextCheck<ApplicationDbContext>();
+
+        services.AddControllersWithViews(options =>
+            options.Filters.Add<ApiExceptionFilterAttribute>())
+                .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+
+        // Customise default API behaviour
+        services.Configure<ApiBehaviorOptions>(options =>
+            options.SuppressModelStateInvalidFilter = true);
+
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo 
+            {
+                Title = "SSW.Rewards API",
+                Version = "v1"
+            });
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter JWT with Bearer into field",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    new string[] {}
+                }
+            });
+        });
+
+        string _allowSpecificOrigins = "_AllowSpecificOrigins";
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy(_allowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins(configuration["AllowedOrigin"])
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+                });
+        });
+
+        services.AddApplicationInsightsTelemetry();
+        services.AddDistributedMemoryCache();
+
+        return services;
+    }
+}
---
+++ src/WebAPI/Program.cs
@@ -0,0 +1,57 @@
+using SSW.Rewards.Infrastructure.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddWebUIServices(builder.Configuration);
+
+builder.Services.AddTransient<ApplicationDbContextInitialiser>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+    await initialiser.InitialiseAsync();
+}
+
+app.UseHealthChecks("/health");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseSwaggerUI(settings =>
+{
+    settings.SwaggerEndpoint("/api", "v1");
+});
+
+app.UseRouting();
+
+string _allowSpecificOrigins = "_AllowSpecificOrigins";
+app.UseCors(_allowSpecificOrigins);
+    
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+//app.MapRazorPages();
+
+//app.MapFallbackToFile("index.html"); ;
+
+app.Run();
