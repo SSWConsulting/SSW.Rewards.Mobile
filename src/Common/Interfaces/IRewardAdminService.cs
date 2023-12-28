f5a79c4dd6066a91e149e20a9a76c64109001a53
Matt Goldman <matt.goldman@outlook.com>
2023-12-29 10:27:19 +1100

    Finished rewards DTOs and service definition

--- src/Common/Interfaces/IRewardAdminService.cs
+++ src/Common/Interfaces/IRewardAdminService.cs
@@ -6,6 +6,7 @@ public interface IRewardAdminService
 {
     Task<int> AddReward(RewardEditDto reward);
     Task UpdateReward(RewardEditDto reward);
+    Task DeleteReward(int rewardId);
 
     Task<ClaimRewardResult> ClaimForUser(string code, int userId);
 }
