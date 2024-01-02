d8091ae6aa5fad60b0384516274a0d138f8833f6
Matt Goldman <matt.goldman@outlook.com>
2024-01-02 14:11:35 +1100

    Started adding Skills DTOs

---
+++ src/Common/Interfaces/ISkillsAdminService.cs
@@ -0,0 +1,10 @@
+﻿using Shared.DTOs.Skills;
+
+namespace Shared.Interfaces;
+
+public interface ISkillsAdminService
+{
+    Task DeleteSkill(string skillId);
+
+    Task<int> AddOrUpdateSkill(SkillDto skill);
+}
