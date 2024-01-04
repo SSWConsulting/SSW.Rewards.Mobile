916053be4b6e40bdb531c8e8dc388af6f5cd0905
Matt Goldman <matt.goldman@outlook.com>
2024-01-04 12:25:45 +1100

    Cleaned up legacy files

--- src/Core/Application/Achievements/Command/ClaimAchievement/ClaimAchievementCommandValidator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SSW.Rewards.Application.Achievements.Command.ClaimAchievement;
-
-public class ClaimAchievementCommandValidator : AbstractValidator<ClaimAchievementCommand>
-{
-    public ClaimAchievementCommandValidator()
-    {
-        RuleFor(x => x.Code).NotEmpty().MaximumLength(128);
-    }
-}
