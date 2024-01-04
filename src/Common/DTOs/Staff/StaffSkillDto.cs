2c624f88155397c3a20c498492be06a2ac54ce43
Matt Goldman <matt.goldman@outlook.com>
2024-01-04 12:59:10 +1100

    Added staff member dtos and interface definitions

---
+++ src/Common/DTOs/Staff/StaffSkillDto.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Models;
+
+namespace Shared.DTOs.Staff;
+public class StaffSkillDto
+{
+    public required string Name { get; set; }
+
+    public SkillLevel Level { get; set; }
+}
---
+++ src/Common/Interfaces/IStaffAdminService.cs
@@ -0,0 +1,12 @@
+﻿using Shared.DTOs.Staff;
+
+namespace Shared.Interfaces;
+
+public interface IStaffAdminService
+{
+    Task DeleteStaffMember(int id, CancellationToken cancellationToken);
+
+    Task UploadProfilePicture(int id, Stream file, CancellationToken cancellationToken);
+
+    Task<StaffMemberDto> UpsertStaffMemberProfile(StaffMemberDto dto, CancellationToken cancellationToken);
+}
