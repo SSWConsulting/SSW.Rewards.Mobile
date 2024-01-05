1d2edc3cecac38e950116a242c230d287f837668
Matt Goldman <matt.goldman@outlook.com>
2024-01-05 17:56:55 +1100

    Implemented staff service

--- src/ApiClient/Services/IStaffService.cs
+++ src/ApiClient/Services/IStaffService.cs
@@ -1,4 +1,5 @@
-﻿using SSW.Rewards.Shared.DTOs.Staff;
+﻿using System.Net.Http.Json;
+using SSW.Rewards.Shared.DTOs.Staff;
 
 namespace SSW.Rewards.ApiClient.Services;
 
@@ -9,4 +10,73 @@ public interface IStaffService
     Task<StaffMemberDto> GetStaffMember(int id, CancellationToken cancellationToken);
 
     Task<StaffMemberDto> SearchStaffMember(StaffMemberQueryDto query, CancellationToken cancellationToken);
+}
+
+public class StaffService : IStaffService
+{
+    private readonly HttpClient _httpClient;
+
+    private const string _baseRoute = "api/Staff/";
+
+    public StaffService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<StaffListViewModel> GetStaffList(CancellationToken cancellationToken)
+    {
+        var result = await _httpClient.GetAsync($"{_baseRoute}", cancellationToken);
+
+        if  (result.IsSuccessStatusCode)
+        {
+            var response = await result.Content.ReadFromJsonAsync<StaffListViewModel>(cancellationToken: cancellationToken);
+
+            if (response is not null)
+            {
+                return response;
+            }
+        }
+
+        var responseContent = await result.Content.ReadAsStringAsync(cancellationToken);
+
+        throw new Exception($"Failed to get staff list: {responseContent}");
+    }
+
+    public async Task<StaffMemberDto> GetStaffMember(int id, CancellationToken cancellationToken)
+    {
+        var result = await _httpClient.GetAsync($"{_baseRoute}{id}", cancellationToken);
+
+        if  (result.IsSuccessStatusCode)
+        {
+            var response = await result.Content.ReadFromJsonAsync<StaffMemberDto>(cancellationToken: cancellationToken);
+
+            if (response is not null)
+            {
+                return response;
+            }
+        }
+
+        var responseContent = await result.Content.ReadAsStringAsync(cancellationToken);
+
+        throw new Exception($"Failed to get staff member: {responseContent}");
+    }
+
+    public async Task<StaffMemberDto> SearchStaffMember(StaffMemberQueryDto query, CancellationToken cancellationToken)
+    {
+        var result = await _httpClient.PostAsJsonAsync($"{_baseRoute}Search", query, cancellationToken);
+
+        if  (result.IsSuccessStatusCode)
+        {
+            var response = await result.Content.ReadFromJsonAsync<StaffMemberDto>(cancellationToken: cancellationToken);
+
+            if (response is not null)
+            {
+                return response;
+            }
+        }
+
+        var responseContent = await result.Content.ReadAsStringAsync(cancellationToken);
+
+        throw new Exception($"Failed to search staff member: {responseContent}");
+    }
 }
