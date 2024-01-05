7a116f2711f9e3d929500550f1db63c868bbb664
Matt Goldman <matt.goldman@outlook.com>
2024-01-05 17:48:18 +1100

    Implemented skills admin service

--- src/ApiClient/Services/ISkillsAdminService.cs
+++ src/ApiClient/Services/ISkillsAdminService.cs
@@ -1,4 +1,5 @@
-﻿using SSW.Rewards.Shared.DTOs.Skills;
+﻿using System.Net.Http.Json;
+using SSW.Rewards.Shared.DTOs.Skills;
 
 namespace SSW.Rewards.ApiClient.Services;
 
@@ -7,4 +8,44 @@ public interface ISkillsAdminService
     Task DeleteSkill(int skillId, CancellationToken cancellationToken);
 
     Task<int> AddOrUpdateSkill(SkillDto skill, CancellationToken cancellationToken);
+}
+
+public class SkillsAdminService : ISkillsAdminService
+{
+    private readonly HttpClient _httpClient;
+
+    private const string _baseRoute = "api/Skills/";
+
+    public SkillsAdminService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task DeleteSkill(int skillId, CancellationToken cancellationToken)
+    {
+        var result = await _httpClient.DeleteAsync($"{_baseRoute}{skillId}", cancellationToken);
+
+        if  (result.IsSuccessStatusCode)
+        {
+            return;
+        }
+
+        var responseContent = await result.Content.ReadAsStringAsync(cancellationToken);
+        throw new Exception($"Failed to delete skill: {responseContent}");
+    }
+
+    public async Task<int> AddOrUpdateSkill(SkillDto skill, CancellationToken cancellationToken)
+    {
+        var result = await _httpClient.PutAsJsonAsync($"{_baseRoute}", skill, cancellationToken);
+
+        if  (result.IsSuccessStatusCode)
+        {
+            var response = await result.Content.ReadFromJsonAsync<int>(cancellationToken: cancellationToken);
+
+            return response;
+        }
+
+        var responseContent = await result.Content.ReadAsStringAsync(cancellationToken);
+        throw new Exception($"Failed to add or update skill: {responseContent}");
+    }
 }
