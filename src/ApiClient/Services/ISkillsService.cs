7b122d735a1698f65496b91d101a90df7c8ab9d5
Matt Goldman <matt.goldman@outlook.com>
2024-01-05 17:51:37 +1100

    Implemented skills service

--- src/ApiClient/Services/ISkillsService.cs
+++ src/ApiClient/Services/ISkillsService.cs
@@ -1,8 +1,39 @@
-﻿using SSW.Rewards.Shared.DTOs.Skills;
+﻿using System.Net.Http.Json;
+using SSW.Rewards.Shared.DTOs.Skills;
 
 namespace SSW.Rewards.ApiClient.Services;
 
 public interface ISkillsService
 {
     Task<SkillsListViewModel> GetSkillsList(CancellationToken cancellationToken);
+}
+
+public class SkillsService : ISkillsService
+{
+    private readonly HttpClient _httpClient;
+
+    private const string _baseRoute = "api/Skills/";
+
+    public SkillsService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public async Task<SkillsListViewModel> GetSkillsList(CancellationToken cancellationToken)
+    {
+        var result = await _httpClient.GetAsync($"{_baseRoute}", cancellationToken);
+
+        if  (result.IsSuccessStatusCode)
+        {
+            var response = await result.Content.ReadFromJsonAsync<SkillsListViewModel>(cancellationToken: cancellationToken);
+
+            if (response is not null)
+            {
+                return response;
+            }
+        }
+
+        var responseContent = await result.Content.ReadAsStringAsync(cancellationToken);
+        throw new Exception($"Failed to get skills list: {responseContent}");
+    }
 }
