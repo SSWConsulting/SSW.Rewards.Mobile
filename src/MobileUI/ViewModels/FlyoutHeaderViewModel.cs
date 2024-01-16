27d146ab308bf0676c3c4bb1b1cf6c3132f5426e
Zachary Keeping <zacharykeeping@icloud.com>
2024-01-16 12:00:35 +1100

    Load ranking

--- src/MobileUI/ViewModels/FlyoutHeaderViewModel.cs
+++ src/MobileUI/ViewModels/FlyoutHeaderViewModel.cs
@@ -6,6 +6,9 @@ namespace SSW.Rewards.Mobile.ViewModels;
 
 public partial class FlyoutHeaderViewModel : ObservableObject, IRecipient<UserDetailsUpdatedMessage>
 {
+    private readonly IUserService _userService;
+    private readonly ILeaderService _leaderService;
+    
     [ObservableProperty]
     private string _profilePic;
 
@@ -26,10 +29,15 @@ public partial class FlyoutHeaderViewModel : ObservableObject, IRecipient<UserDe
     
     [ObservableProperty]
     private int _credits;
+    
+    [ObservableProperty]
+    private int _myRank;
 
-    public FlyoutHeaderViewModel(IUserService userService)
+    public FlyoutHeaderViewModel(IUserService userService, ILeaderService leaderService)
     {
         WeakReferenceMessenger.Default.Register<UserDetailsUpdatedMessage>(this);
+        _userService = userService;
+        _leaderService = leaderService;
         ProfilePic = userService.MyProfilePic;
         Name = userService.MyName;
         Email = userService.MyEmail;
@@ -38,6 +46,15 @@ public partial class FlyoutHeaderViewModel : ObservableObject, IRecipient<UserDe
         QrCode = userService.MyQrCode;
         Points = userService.MyPoints;
         Credits = userService.MyBalance;
+
+        _ = LoadRank();
+    }
+    
+    private async Task LoadRank()
+    {
+        var summaries = await _leaderService.GetLeadersAsync(false);
+        var myId = _userService.MyUserId;
+        MyRank = summaries.FirstOrDefault(x => x.UserId == myId)?.Rank ?? 0;
     }
 
     public void Receive(UserDetailsUpdatedMessage message)
