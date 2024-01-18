a8d47bfbde91f3c5dad78832ccbfa023754696c5
Zachary Keeping <zacharykeeping@icloud.com>
2024-01-19 09:13:18 +1100

    Add animation to leaderboard

--- src/MobileUI/Pages/LeaderboardPage.xaml.cs
+++ src/MobileUI/Pages/LeaderboardPage.xaml.cs
@@ -16,11 +16,25 @@ public partial class LeaderboardPage : ContentPage
     {
         base.OnAppearing();
         await _viewModel.Initialise();
+        await Animate();
         _viewModel.ScrollTo += ScrollTo;
     }
 
     private void ScrollTo(int i)
     {
         LeadersCollection.ScrollTo(i, position: ScrollToPosition.Center);
+    }
+
+    private async Task Animate()
+    {
+        LeadersCollection.Opacity = 0;
+        LeadersCollection.TranslationY = 400;
+        
+        await Task.WhenAll
+        (
+            LeadersCollection.FadeTo(1, 1000, Easing.SinIn),
+        	LeadersCollection.TranslateTo(0, 0, 1000, Easing.SinIn)
+        );
+
     }
 }
