4b47ffb61928763a009acb688718b5580a34e46f
Zachary Keeping <zacharykeeping@icloud.com>
2024-01-22 14:21:01 +1100

    Add Earn page with collection view styling

---
+++ src/MobileUI/Pages/EarnPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace SSW.Rewards.Mobile.Pages;
+
+public partial class EarnPage : ContentPage
+{
+    private readonly EarnViewModel _viewModel;
+
+    public EarnPage(EarnViewModel viewModel)
+    {
+        InitializeComponent();
+        _viewModel = viewModel;
+        BindingContext = _viewModel;
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _viewModel.Initialise();
+    }
+}
