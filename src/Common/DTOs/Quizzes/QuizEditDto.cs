50a8cbe67d895698f688235bf907cbf9e1e70bf1
Tylah Kapa <tylahkapa@gmail.com>
2024-01-24 12:21:26 +1000

    UI - Add Carousel Image

--- src/Common/DTOs/Quizzes/QuizEditDto.cs
+++ src/Common/DTOs/Quizzes/QuizEditDto.cs
@@ -5,6 +5,8 @@ public class QuizEditDto
     public int QuizId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public string? CarouselImage { get; set; } = string.Empty;
+    public bool IsCarousel { get; set; } = false;
     public int Points { get; set; }
     public Icons Icon { get; set; }
     public bool IsArchived { get; set; }
