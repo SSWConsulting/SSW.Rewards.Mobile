c90492990f2e9c0bb96fbc05d97839881fe0b258
Tylah Kapa <tylahkapa@gmail.com>
2024-01-29 17:34:00 +1000

    Add Thumbnail and CarouselImages to GetQuizListForUser

--- src/Common/DTOs/Quizzes/QuizDto.cs
+++ src/Common/DTOs/Quizzes/QuizDto.cs
@@ -6,5 +6,10 @@ public class QuizDto
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public bool Passed { get; set; } = false;
+    public string ThumbnailImage { get; set; } = string.Empty;
+    public string CarouselImage { get; set; } = string.Empty;
+    public bool IsCarousel { get; set; } = false;
+    
+    // TODO: Remove once the ThumbnailImages are working
     public Icons Icon { get; set; }
 }
