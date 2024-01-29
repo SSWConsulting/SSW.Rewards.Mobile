726a426dc3ffa5ef1cc45981b8c97bde31af006b
Tylah Kapa <tylahkapa@gmail.com>
2024-01-29 15:07:01 +1000

    Add Migration for Files

---
+++ src/Infrastructure/Persistence/Migrations/20240129004821_AddQuizImages.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SSW.Rewards.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddQuizImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CarouselPhoto",
+                table: "Quizzes",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCarousel",
+                table: "Quizzes",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ThumbnailPhoto",
+                table: "Quizzes",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CarouselPhoto",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "IsCarousel",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "ThumbnailPhoto",
+                table: "Quizzes");
+        }
+    }
+}
