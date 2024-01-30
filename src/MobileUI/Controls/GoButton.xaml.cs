77d174346e0076b92af37dc681f33b25102c639e
Zachary Keeping <zacharykeeping@icloud.com>
2024-01-30 16:04:46 +1100

    Add carousel to Earn page

---
+++ src/MobileUI/Controls/GoButton.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Maui.BindableProperty.Generator.Core;
+
+namespace SSW.Rewards.Mobile.Controls;
+
+public partial class GoButton : ContentView
+{
+    [AutoBindable]
+    private int _points;
+    
+	public GoButton()
+	{
+		InitializeComponent();
+	}
+}
--- src/MobileUI/ViewModels/EarnViewModel.cs
+++ src/MobileUI/ViewModels/EarnViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Messaging;
 using SSW.Rewards.Enums;
 using SSW.Rewards.Mobile.Messages;
@@ -7,15 +8,20 @@ using SSW.Rewards.Shared.DTOs.Quizzes;
 
 namespace SSW.Rewards.Mobile.ViewModels;
 
-public class EarnViewModel : BaseViewModel, IRecipient<QuizzesUpdatedMessage>
+public partial class EarnViewModel : BaseViewModel, IRecipient<QuizzesUpdatedMessage>
 {
     private readonly IQuizService _quizService;
 
     private string quizDetailsPageUrl = "earn/details";
 
-    public ObservableCollection<QuizDto> Quizzes { get; set; } = new ObservableCollection<QuizDto>();
+    public ObservableCollection<QuizDto> Quizzes { get; set; } = new ();
+    
+    public ObservableCollection<QuizDto> CarouselQuizzes { get; set; } = new ();
 
     public ICommand OpenQuizCommand { get; set; }
+
+    [ObservableProperty]
+    private bool _isCarouselVisible;
 
     public EarnViewModel(IQuizService quizService)
     {
@@ -39,23 +45,22 @@ public class EarnViewModel : BaseViewModel, IRecipient<QuizzesUpdatedMessage>
 
     public async Task Initialise()
     {
-        Quizzes = new ObservableCollection<QuizDto>();
-        OnPropertyChanged(nameof(Quizzes));
-
         IsBusy = true;
 
-        OnPropertyChanged(nameof(IsBusy));
-
         var quizzes = await _quizService.GetQuizzes();
-
+        
         foreach (var quiz in quizzes)
         {
             Quizzes.Add(quiz);
+            
+            if (quiz.IsCarousel)
+            {
+                IsCarouselVisible = true;
+                CarouselQuizzes.Add(quiz);
+            }
         }
-
+        
         IsBusy = false;
-
-        OnPropertyChanged(nameof(IsBusy));
     }
 
     private async Task OpenQuiz(int quizId, Icons icon)
