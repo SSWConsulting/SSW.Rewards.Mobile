7a9f5809a14b26b5aeb92041ac9190c6016566ab
Zach Keeping [SSW] <11418832+zacharykeeping@users.noreply.github.com>
2024-02-02 11:17:45 +1100

    ♻️ Mobile - V3 Earn page improvements (#608)
    * Add autoscroll to Earn carousel
    
    * Tighten margins on Earn page
    
    * Adjust indicator size to better match V3 designs
    
    * Adjust quiz carousel positioning
    
    * Change quiz Editor to Entry to resolve issues
    
    * Add disabled state to button
    
    * Center description text on carousel
    
    * Add animation to Earn page

--- src/MobileUI/Pages/EarnPage.xaml.cs
+++ src/MobileUI/Pages/EarnPage.xaml.cs
@@ -4,6 +4,10 @@ public partial class EarnPage : ContentPage
 {
     private readonly EarnViewModel _viewModel;
 
+    private IDispatcherTimer _timer;
+    
+    private bool _isLoaded;
+
     public EarnPage(EarnViewModel viewModel)
     {
         InitializeComponent();
@@ -15,5 +19,56 @@ public partial class EarnPage : ContentPage
     {
         base.OnAppearing();
         await _viewModel.Initialise();
+        await Animate();
+        BeginAutoScroll();
+    }
+    
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        _timer.Stop();
+    }
+
+    private void BeginAutoScroll()
+    {
+        _timer = Application.Current.Dispatcher.CreateTimer();
+        _timer.Interval = TimeSpan.FromSeconds(3);
+        _timer.Tick += (s,e) => Scroll();
+        _timer.Start();
+    }
+    
+    private void Scroll()
+    {
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            var count = _viewModel.CarouselQuizzes.Count;
+            
+            if (count > 0)
+                Carousel.Position = (Carousel.Position + 1) % count;
+        });
+    }
+    
+    private async Task Animate()
+    {
+        if (_isLoaded)
+        {
+            return;
+        }
+        
+        CarouselSection.Opacity = 0;
+        CarouselSection.TranslationY = -400;
+        
+        QuizListSection.Opacity = 0;
+        QuizListSection.TranslationY = 400;
+        
+        await Task.WhenAll
+        (
+            CarouselSection.FadeTo(1, 600, Easing.CubicIn),
+            QuizListSection.FadeTo(1, 600, Easing.CubicIn),
+            CarouselSection.TranslateTo(0, 0, 600, Easing.SinIn),
+            QuizListSection.TranslateTo(0, 0, 600, Easing.SinIn)
+        );
+        
+        _isLoaded = true;
     }
 }
