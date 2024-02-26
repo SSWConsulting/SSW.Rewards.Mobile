ecf1b7d3a0aee5b6ddd99ab11019618dab428b47
Tylah Kapa <tylahkapa@gmail.com>
2024-02-26 13:40:48 +1000

    AddSkillUri Migration

---
+++ src/Infrastructure/Persistence/Migrations/20240226034012_AddSkillUri.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SSW.Rewards.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSkillUri : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUri",
+                table: "Skills",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUri",
+                table: "Skills");
+        }
+    }
+}
