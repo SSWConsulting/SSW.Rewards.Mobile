608c3aaa31c8cc77685b12c20bbf9aa3c7306047
Tylah Kapa <tylahkapa@gmail.com>
2024-02-26 14:15:36 +1000

    Add PhotoUploadHelper.cs

---
+++ src/AdminUI/Helpers/PhotoUploadHelper.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace SSW.Rewards.Admin.UI.Helpers;
+
+public class PhotoUploadHelper
+{
+    public static async Task<(string base64String, string fileName)> UploadPhoto(InputFileChangeEventArgs e)
+    {
+        var imageFile = e.File;
+
+        var ms = new MemoryStream();
+
+        await imageFile.OpenReadStream().CopyToAsync(ms);
+
+        var bytes = ms.ToArray();
+
+        return (Convert.ToBase64String(bytes), imageFile.Name);
+    }
+}
