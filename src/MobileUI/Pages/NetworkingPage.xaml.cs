b2d04e95ea42ce2cac9c53a9727d6df5242540bc
Tylah Kapa <tylahkapa@gmail.com>
2024-02-29 15:55:35 +1000

    Create NetworkingPage.xaml and Viewmodel. Display Segments.

---
+++ src/MobileUI/Pages/NetworkingPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace SSW.Rewards.Mobile.Pages;
+
+public partial class NetworkingPage : ContentPage
+{
+    private readonly NetworkingPageViewModel _viewModel;
+
+    public NetworkingPage(NetworkingPageViewModel viewModel)
+    {
+        _viewModel = viewModel;
+        _viewModel.Navigation = Navigation;
+        BindingContext = _viewModel;
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _viewModel.Initialise();
+    }
+}
