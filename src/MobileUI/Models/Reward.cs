27699a1d0fc0b41a17aebb02330d4f83b645c5c3
Zach Keeping [SSW] <11418832+zacharykeeping@users.noreply.github.com>
2024-03-01 12:15:44 +1100

    ✨ Mobile | V3 Redeem page UI (#743)
    * Initial redeem page code
    
    * Tweak design
    
    * Cleanup
    
    * Extract out ListItem
    
    * Update button disable handling
    
    * Extract out CarouselItem
    
    * Add description to carousel
    
    * Pass id to listitem
    
    * Add placeholders to listitem
    
    * Implement first redeem popup
    
    * Bump mobile build versions (#725)
    
    * Update Sprint Issue Templates (#724)
    
    * Add skill images with backup icon (#726)
    
    * ✨ Mobile | Hide Label and Divider for non-SSW users #728
    
    * Hide navigation bar on iOS and shift topbar upwards. (#732)
    
    * Hide navigation bar on iOS and shift topbar upwards.
    
    * Made scan page modal
    Switched border for button
    Gave Button high z-index
    
    * Add address section to redemption
    
    * Adjust radio design
    
    * Adjust radio colour
    
    ---------
    
    Co-authored-by: Matt Goldman <matt.goldman@outlook.com>
    Co-authored-by: Tylah Kapa [SSW] <tylahkapa@gmail.com>
    Co-authored-by: Anton Polkanov [SSW] <123032112+AntPolkanov@users.noreply.github.com>

--- src/MobileUI/Models/Reward.cs
+++ src/MobileUI/Models/Reward.cs
@@ -4,9 +4,13 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
         public int Cost { get; set; }
         public DateTimeOffset? AwardedAt { get; set; }
         public bool Awarded { get; set; }
         public string ImageUri { get; set; }
+        public string CarouselImageUri { get; set; }
+        public bool IsCarousel { get; set; }
+        public bool CanAfford { get; set; }
     }
 }
---
+++ src/MobileUI/ViewModels/RedeemRewardViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Mopups.Services;
+
+namespace SSW.Rewards.Mobile.ViewModels;
+
+public partial class RedeemRewardViewModel(IUserService userService) : BaseViewModel
+{
+    [ObservableProperty]
+    private string _image;
+    
+    [ObservableProperty]
+    private string _name;
+    
+    [ObservableProperty]
+    private string _description;
+    
+    [ObservableProperty]
+    private int _cost;
+    
+    [ObservableProperty]
+    private int _userBalance;
+
+    [ObservableProperty]
+    private bool _isBalanceVisible = true;
+    
+    [ObservableProperty]
+    private bool _isAddressVisible;
+    
+    public string AddressLine1 { get; set; }
+    public string AddressLine2 { get; set; }
+    public string AddressSuburb { get; set; }
+    public string AddressState { get; set; }
+    public string AddressPostcode { get; set; }
+    
+    public ObservableCollection<string> States { get; set; } =
+    [
+        "NSW",
+        "VIC",
+        "QLD",
+        "SA",
+        "WA",
+        "TAS",
+        "NT",
+        "ACT"
+    ];
+
+    public void Initialise(Reward reward)
+    {
+        Image = reward.ImageUri;
+        Name = reward.Name;
+        Description = reward.Description;
+        Cost = reward.Cost;
+        
+        UserBalance = userService.MyBalance;
+    }
+
+    [RelayCommand]
+    private void ClosePopup()
+    {
+        MopupService.Instance.PopAsync();
+    }
+
+    [RelayCommand]
+    private void NextClicked()
+    {
+        IsBalanceVisible = false;
+        IsAddressVisible = true;
+    }
+    
+    [RelayCommand]
+    private void ConfirmClicked()
+    {
+    }
+}
