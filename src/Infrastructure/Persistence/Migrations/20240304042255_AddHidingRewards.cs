0c16001a6f83a19dd718f6cfe2f1e855aba4ca12
Tylah Kapa [SSW] <tylahkapa@gmail.com>
2024-03-05 10:14:39 +1000

    ✨ Admin/API | Adminui disable rewards (#748)
    * AddHidingRewards Migration
    
    * Update Reward
    
    * Update Commands
    
    * Update Rewards forms with Hidden On Mobile.

---
+++ src/Infrastructure/Persistence/Migrations/20240304042255_AddHidingRewards.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SSW.Rewards.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHidingRewards : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsHidden",
+                table: "Rewards",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsHidden",
+                table: "Rewards");
+        }
+    }
+}
