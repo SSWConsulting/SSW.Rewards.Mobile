b36b501fa7ed6171743b1ea5fd12e40d69c8ccbb
Tylah Kapa <tylahkapa@gmail.com>
2024-03-08 11:48:05 +1000

    Revert "Add AutoMapper"
    This reverts commit 491f6513

--- src/Application/Network/Queries/GetNetworkProfileList/GetNetworkProfileListQuery.cs
+++ src/Application/Network/Queries/GetNetworkProfileList/GetNetworkProfileListQuery.cs
@@ -1,5 +1,4 @@
 using AutoMapper.QueryableExtensions;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SSW.Rewards.Shared.DTOs.Leaderboard;
 using SSW.Rewards.Shared.DTOs.Network;
 using SSW.Rewards.Shared.DTOs.Users;
@@ -26,48 +25,68 @@ public class GetNetworkProfileListHandler : IRequestHandler<GetNetworkProfileLis
     
     public async Task<NetworkProfileListViewModel> Handle(GetNetworkProfileListQuery request, CancellationToken cancellationToken)
     {
-        var profiles = new List<NetworkProfileDto>(200);
-
-        var leaderboardList = _dbContext.Users
-            .Where(u => u.Activated && !string.IsNullOrWhiteSpace(u.FullName))
-            .Include(u => u.UserAchievements)
-            .ThenInclude(ua => ua.Achievement)
-            .ProjectTo<LeaderboardUserDto>(_mapper.ConfigurationProvider)
-            .OrderByDescending(u => u.TotalPoints)
-            .ToList()
-            .Select((u, i) =>
-            {
-                u.Rank = i + 1;
-                return u;
-            })
-            .ToList();
-        
+        var profiles = new List<NetworkProfileDto>();
         var user = await _userService.GetCurrentUser(cancellationToken);
         var achievements = await _userService.GetUserAchievements(user.Id, cancellationToken);
-        
-        var networkProfileDtos = await _dbContext.StaffMembers
-            .Where(staff => !staff.IsDeleted && staff.Email != user.Email)
-            .Join(_dbContext.Users,
-                staff => staff.Email, 
-                user => user.Email,
-                (staff, user) => new NetworkMap { Staff = staff, User = user })
-            .ProjectTo<NetworkProfileDto>(_mapper.ConfigurationProvider)
-            .ToListAsync(cancellationToken);
         
         var completedAchievements = achievements.UserAchievements
             .Where(a => a.Complete)
             .Select(a => a.AchievementId)
             .ToList();
-        
-        profiles.AddRange(networkProfileDtos.Select(profile =>
-        {
-            var leaderboardUser = leaderboardList.FirstOrDefault(u => u.UserId == profile.UserId);
-            
-            profile.TotalPoints = leaderboardUser.TotalPoints;
-            profile.Rank = leaderboardUser.Rank;
-            profile.Scanned = (bool)(completedAchievements?.Contains(profile.AchievementId));
 
-            return profile;
+        var staffDtos = await _dbContext.StaffMembers
+            .Join(_dbContext.Users,
+                staff => staff.Email, 
+                user => user.Email,
+                (staff, user) =>
+                new
+                {
+                    UserId = user.Id,
+                    staff.Name,
+                    ProfilePicture = user.Avatar ?? "v2sophie",
+                    staff.Title,
+                    staff.Email,
+                    AchievementId = staff.StaffAchievement.Id,
+                    staff.IsDeleted,
+                    user.Activated
+                })
+            .Where(x => (!x.IsDeleted && x.Activated) && x.UserId != user.Id)
+            .ToListAsync(cancellationToken);
+        
+        var users = await _dbContext.Users
+            .Where(u => u.Activated)
+            .Include(u => u.UserAchievements)
+            .ThenInclude(ua => ua.Achievement)
+            .ProjectTo<LeaderboardUserDto>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        // need to set rank outside of AutoMapper
+        var leaderboardUserDtos = users
+            .Where(u => !string.IsNullOrWhiteSpace(u.Name))
+            .OrderByDescending(u => u.TotalPoints)
+            .Select((u, i) =>
+            {
+                u.Rank = i + 1;
+                return u;
+            }).ToList();
+        
+        profiles.AddRange(staffDtos.Select(profile =>
+        {
+            var leaderboardUser = leaderboardUserDtos.FirstOrDefault(u => u.UserId == profile.UserId);
+            
+            return new NetworkProfileDto
+            {
+                UserId = profile.UserId,
+                ProfilePicture = profile.ProfilePicture,
+                TotalPoints = leaderboardUser.TotalPoints,
+                Rank = leaderboardUser.Rank,
+                IsExternal = false,
+                Name = profile.Name,
+                Title = profile.Title,
+                Email = profile.Email,
+                AchievementId = profile.AchievementId,
+                Scanned = (bool)(completedAchievements?.Contains(profile.AchievementId)),
+            };
         }));
         
         return new NetworkProfileListViewModel { Profiles = profiles };
