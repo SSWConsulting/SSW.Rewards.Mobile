4a26c29263df21a6e5685c1867b15dafc8d16cee
AntPolkanov <ap.polkanov@gmail.com>
2024-03-12 10:10:40 +1100

    delete interface

--- src/Application/Common/Interfaces/IActivityFeedService.cs
+++
@@ -1,8 +0,0 @@
-using SSW.Rewards.Shared.DTOs.ActivityFeed;
-
-namespace SSW.Rewards.Application.Common.Interfaces;
-
-public interface IActivityFeedService
-{
-    public Task<IList<ActivityFeedViewModel>> GetActivities(ActivityFeedFilter filter, int skip, int take, CancellationToken cancellationToken);
-}
