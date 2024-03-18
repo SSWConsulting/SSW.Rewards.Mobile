6bc0e872063ea47742d04c952a7a336acc937531
Tylah Kapa <tylahkapa@gmail.com>
2024-03-18 16:15:36 +1000

    Make fields nullable

--- src/Common/DTOs/ActivityFeed/ActivityFeedViewModel.cs
+++ src/Common/DTOs/ActivityFeed/ActivityFeedViewModel.cs
@@ -4,8 +4,8 @@ namespace SSW.Rewards.Shared.DTOs.ActivityFeed;
 
 public class ActivityFeedViewModel
 {
-    public string UserAvatar { get; set; }
-    public string UserName { get; set; }
+    public string? UserAvatar { get; set; }
+    public string? UserName { get; set; }
     public UserAchievementDto Achievement { get; set; }
     public DateTime AwardedAt { get; set; }
 }
