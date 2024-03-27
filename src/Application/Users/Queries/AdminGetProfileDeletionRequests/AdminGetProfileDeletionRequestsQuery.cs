542102e79923004b7476142dc4c75c3578eb117f
Matt Goldman <matt.goldman@outlook.com>
2024-03-27 16:51:55 +1100

    Completed command and query endpoints

---
+++ src/Application/Users/Queries/AdminGetProfileDeletionRequests/AdminGetProfileDeletionRequestsQuery.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper.QueryableExtensions;
+using SSW.Rewards.Shared.DTOs.Users;
+
+namespace SSW.Rewards.Application.Users.Queries.AdminGetProfileDeletionRequests;
+
+
+public class AdminGetProfileDeletionRequestsQuery : IRequest<ProfileDeletionRequestsVieWModel> { }
+
+public class AdminProfileDeletionRequestsQueryHandler : IRequestHandler<AdminGetProfileDeletionRequestsQuery, ProfileDeletionRequestsVieWModel>
+{
+    private readonly IApplicationDbContext _dbContext;
+    private readonly IMapper _mapper;
+
+    public AdminProfileDeletionRequestsQueryHandler(IApplicationDbContext dbContext, IMapper mapper)
+    {
+        _dbContext = dbContext;
+        _mapper = mapper;
+    }
+
+    public async Task<ProfileDeletionRequestsVieWModel> Handle(AdminGetProfileDeletionRequestsQuery request, CancellationToken cancellationToken)
+    {
+        var requests = await _dbContext.OpenProfileDeletionRequests
+            .ProjectTo<ProfileDeletionRequestDto>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        return new ProfileDeletionRequestsVieWModel
+        {
+            Requests = requests
+        };
+    }
+}
---
+++ src/Common/DTOs/Users/ProfileDeletionRequestDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SSW.Rewards.Shared.DTOs.Users;
+
+public class ProfileDeletionRequestDto
+{
+    public int UserId { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Requested { get; set; }
+}
+
+public class ProfileDeletionRequestsVieWModel
+{
+    public List<ProfileDeletionRequestDto> Requests { get; set; } = new();
+}
