ad285b00301d87f3add995e0ea2d657aef27cf54
Tylah Kapa <tylahkapa@gmail.com>
2024-04-05 16:30:16 +1000

    Save Progress

---
+++ src/Infrastructure/Persistence/Migrations/20240405061000_UpdateAchievementCodes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SSW.Rewards.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAchievementCodes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //TODO: Update to Base64
+            migrationBuilder.Sql("UPDATE Achievements SET Code = 'ach:' + CONVERT(NVARCHAR(36), NEWID())");
+        }
+    }
+}
