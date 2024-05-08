1edc9774a2bf5b9a38c6d7a1f0b218af961ebddc
Zachary Keeping <zacharykeeping@icloud.com>
2024-05-08 14:12:08 +1000

    Revert delay

--- src/MobileUI/ViewModels/EarnDetailsViewModel.cs
+++ src/MobileUI/ViewModels/EarnDetailsViewModel.cs
@@ -174,8 +174,8 @@ namespace SSW.Rewards.Mobile.ViewModels
 
         private async Task<bool> AwaitQuizCompletion()
         {
-            var maxAttempts = 60;
-            var delay = 2000;
+            var maxAttempts = 30;
+            var delay = 5000;
             bool isComplete = false;
 
             while (!isComplete)
