a66ef6d50553f1a580d45a6fe2817dff6b3e5f8c
Zachary Keeping <zacharykeeping@icloud.com>
2024-05-09 09:07:24 +1000

    Create MultiLineButton to fix issues with buttons with multiple lines on iOS

---
+++ src/MobileUI/Controls/MultiLineButton.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Input;
+
+namespace SSW.Rewards.Mobile.Controls;
+
+public partial class MultiLineButton
+{
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public static readonly BindableProperty TextProperty =
+        BindableProperty.Create(
+            nameof(Text),
+            typeof(string),
+            typeof(MultiLineButton),
+            string.Empty
+        );
+    
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
+    public static readonly BindableProperty CommandProperty =
+        BindableProperty.Create(
+            nameof(Command),
+            typeof(ICommand),
+            typeof(MultiLineButton)
+        );
+    
+    public MultiLineButton()
+    {
+        InitializeComponent();
+    }
+}
