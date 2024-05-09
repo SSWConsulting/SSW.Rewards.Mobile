0ab19f7f8fba21a91cc776a5f4d14b4c4e73d2d2
Zachary Keeping <zacharykeeping@icloud.com>
2024-05-09 10:12:51 +1000

    Allow setting colors for MultiLineButton

--- src/MobileUI/Controls/MultiLineButton.xaml.cs
+++ src/MobileUI/Controls/MultiLineButton.xaml.cs
@@ -18,6 +18,48 @@ public partial class MultiLineButton
             string.Empty
         );
     
+    public Color TextColor
+    {
+        get => (Color)GetValue(TextColorProperty);
+        set => SetValue(TextColorProperty, value);
+    }
+
+    public static readonly BindableProperty TextColorProperty =
+        BindableProperty.Create(
+            nameof(TextColor),
+            typeof(Color),
+            typeof(MultiLineButton),
+            Colors.White
+        );
+    
+    public int FontSize
+    {
+        get => (int)GetValue(FontSizeProperty);
+        set => SetValue(FontSizeProperty, value);
+    }
+
+    public static readonly BindableProperty FontSizeProperty =
+        BindableProperty.Create(
+            nameof(FontSize),
+            typeof(int),
+            typeof(MultiLineButton),
+            14
+        );
+    
+    public new Color BackgroundColor
+    {
+        get => (Color)GetValue(BackgroundColorProperty);
+        set => SetValue(BackgroundColorProperty, value);
+    }
+
+    public new static readonly BindableProperty BackgroundColorProperty =
+        BindableProperty.Create(
+            nameof(BackgroundColor),
+            typeof(Color),
+            typeof(MultiLineButton),
+            App.Current.Resources["SSWRed"] as Color
+        );
+    
     public ICommand Command
     {
         get => (ICommand)GetValue(CommandProperty);
