b7d272120c9fe9d0bdfdaca9c8740a0f938d6676
Zach Keeping [SSW] <11418832+zacharykeeping@users.noreply.github.com>
2024-05-29 15:05:48 +1000

    🐛 Mobile | Carousel fixes (#931)
    * Fix carousel
    
    * Cleanup
    
    * Fix animation

--- src/MobileUI/Features/Quiz/QuizPage.xaml.cs
+++ src/MobileUI/Features/Quiz/QuizPage.xaml.cs
@@ -1,10 +1,8 @@
 ﻿namespace SSW.Rewards.Mobile.Pages;
 
-public partial class QuizPage : ContentPage
+public partial class QuizPage
 {
     private readonly QuizViewModel _viewModel;
-
-    private IDispatcherTimer _timer;
     
     private bool _isLoaded;
 
@@ -13,7 +11,6 @@ public partial class QuizPage : ContentPage
         InitializeComponent();
         _viewModel = viewModel;
         BindingContext = _viewModel;
-        _timer = Application.Current.Dispatcher.CreateTimer();
     }
 
     protected override async void OnAppearing()
@@ -21,34 +18,6 @@ public partial class QuizPage : ContentPage
         base.OnAppearing();
         await _viewModel.Initialise();
         await Animate();
-        BeginAutoScroll();
-    }
-    
-    protected override void OnDisappearing()
-    {
-        base.OnDisappearing();
-        _timer.Stop();
-        _timer.Tick -= OnScrollTick;
-    }
-
-    private void BeginAutoScroll()
-    {
-        _timer.Interval = TimeSpan.FromSeconds(3);
-        _timer.Tick += OnScrollTick;
-        _timer.Start();
-    }
-    
-    private void OnScrollTick(object sender, object args)
-    {
-        MainThread.BeginInvokeOnMainThread(Scroll);
-    }
-    
-    private void Scroll()
-    {
-        var count = _viewModel.CarouselQuizzes.Count;
-        
-        if (count > 0)
-            Carousel.Position = (Carousel.Position + 1) % count;
     }
     
     private async Task Animate()
@@ -63,6 +32,8 @@ public partial class QuizPage : ContentPage
         
         QuizListSection.Opacity = 0;
         QuizListSection.TranslationY = 400;
+        
+        await Task.Delay(100);
         
         await Task.WhenAll
         (
