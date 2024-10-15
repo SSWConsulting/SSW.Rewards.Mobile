36b8e2ea71f43c1783076d36461a4958dbddbe1a
Zachary Keeping <zacharykeeping@icloud.com>
2024-10-15 14:45:38 +1100

    Update bindings

--- src/MobileUI/Controls/CarouselItem.xaml.cs
+++ src/MobileUI/Controls/CarouselItem.xaml.cs
@@ -1,105 +1,33 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using Maui.BindableProperty.Generator.Core;
 
 namespace SSW.Rewards.Mobile.Controls;
 
 public partial class CarouselItem
 {
-    public string CarouselImage
-    {
-        get => (string)GetValue(CarouselImageProperty);
-        set => SetValue(CarouselImageProperty, value);
-    }
-
-    public static readonly BindableProperty CarouselImageProperty =
-        BindableProperty.Create(
-            nameof(CarouselImage),
-            typeof(string),
-            typeof(CarouselItem),
-            string.Empty
-        );
+    [AutoBindable]
+    private readonly string _carouselImage;
     
-    public string Description
-    {
-        get => (string)GetValue(DescriptionProperty);
-        set => SetValue(DescriptionProperty, value);
-    }
-
-    public static readonly BindableProperty DescriptionProperty =
-        BindableProperty.Create(
-            nameof(Description),
-            typeof(string),
-            typeof(CarouselItem),
-            string.Empty
-        );
+    [AutoBindable]
+    private readonly string _description;
     
-    public int Points
-    {
-        get => (int)GetValue(PointsProperty);
-        set => SetValue(PointsProperty, value);
-    }
-
-    public static readonly BindableProperty PointsProperty =
-        BindableProperty.Create(
-            nameof(Points),
-            typeof(int),
-            typeof(CarouselItem),
-            0
-        );
+    [AutoBindable]
+    private readonly int _points;
     
-    public string ButtonText
-    {
-        get => (string)GetValue(ButtonTextProperty);
-        set => SetValue(ButtonTextProperty, value);
-    }
-
-    public static readonly BindableProperty ButtonTextProperty =
-        BindableProperty.Create(
-            nameof(ButtonText),
-            typeof(string),
-            typeof(CarouselItem),
-            string.Empty
-        );
+    [AutoBindable]
+    private readonly string _buttonText;
     
-    public IAsyncRelayCommand ButtonCommand
-    {
-        get => (IAsyncRelayCommand)GetValue(ButtonCommandProperty);
-        set => SetValue(ButtonCommandProperty, value);
-    }
-
-    public static readonly BindableProperty ButtonCommandProperty =
-        BindableProperty.Create(
-            nameof(ButtonCommand),
-            typeof(IAsyncRelayCommand),
-            typeof(CarouselItem)
-        );
+    [AutoBindable]
+    private readonly IAsyncRelayCommand _buttonCommand;
     
-    public int ItemId
-    {
-        get => (int)GetValue(ItemIdProperty);
-        set => SetValue(ItemIdProperty, value);
-    }
-
-    public static readonly BindableProperty ItemIdProperty =
-        BindableProperty.Create(
-            nameof(ItemId),
-            typeof(int),
-            typeof(CarouselItem),
-            -1
-        );
+    [AutoBindable]
+    private readonly int _itemId;
     
-    public bool IsButtonDisabled
-    {
-        get => (bool)GetValue(IsButtonDisabledProperty);
-        set => SetValue(IsButtonDisabledProperty, value);
-    }
-
-    public static readonly BindableProperty IsButtonDisabledProperty =
-        BindableProperty.Create(
-            nameof(IsButtonDisabled),
-            typeof(bool),
-            typeof(CarouselItem),
-            false
-        );
+    [AutoBindable]
+    private readonly bool _isButtonDisabled;
+    
+    [AutoBindable]
+    private readonly string _ribbonText;
     
     private object _cachedBindingContext;
 
