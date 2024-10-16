ce2d6f6ff79a091b78b5643dc3ee7fd6c8bc583d
Zach Keeping [SSW] <11418832+zacharykeeping@users.noreply.github.com>
2024-10-16 13:40:03 +1100

    Fix top bar (#1089)

--- src/MobileUI/Features/AppShell.xaml.cs
+++ src/MobileUI/Features/AppShell.xaml.cs
@@ -2,6 +2,10 @@
 using CommunityToolkit.Mvvm.Messaging;
 using SSW.Rewards.Mobile.Messages;
 
+#if IOS
+using UIKit;
+#endif
+
 namespace SSW.Rewards.Mobile;
 
 public partial class AppShell
@@ -23,6 +27,38 @@ public partial class AppShell
 
         Process.GetCurrentProcess().CloseMainWindow();
         return true;
+    }
+    
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		
+#if IOS
+        // Workaround for filling the gap above our custom top bar on iOS
+        // See: https://github.com/drasticactions/MauiRepros/blob/main/StatusBarHack/MainPage.xaml.cs#L30-L52
+        if (this.GetParentWindow()?.Handler?.PlatformView is not UIWindow window)
+        {
+            return;
+        }
+
+        var topPadding = window?.SafeAreaInsets.Top ?? 0;
+        Application.Current.Resources.TryGetValue("Background", out var background);
+        
+        if (background is not Color backgroundColor)
+        {
+            return;
+        }
+
+        var statusBar = new UIView(new CoreGraphics.CGRect(0, 0, UIKit.UIScreen.MainScreen.Bounds.Size.Width, topPadding))
+        {
+            BackgroundColor = UIColor.FromRGB(backgroundColor.Red, backgroundColor.Green, backgroundColor.Blue)
+        };
+
+        if (this.Handler?.PlatformView is UIView view)
+        {
+            view?.AddSubview(statusBar);
+        }
+#endif
     }
 
     private void OnNavigating(object sender, ShellNavigatingEventArgs e)
