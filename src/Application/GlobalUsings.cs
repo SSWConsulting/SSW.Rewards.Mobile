4b698c432f1c04d476d8d9b84642283b0c686562
Jernej Kavka (JK) [SSW • Microsoft MVP] <JernejKavka@ssw.com.au>
2025-01-15 17:33:50 +1000

    Adding TagWithContext method for better SQL telemetry

--- src/Application/GlobalUsings.cs
+++ src/Application/GlobalUsings.cs
@@ -1,7 +1,8 @@
-﻿global using MediatR;
+﻿global using AutoMapper;
 global using FluentValidation;
+global using MediatR;
 global using Microsoft.EntityFrameworkCore;
+global using SSW.Rewards.Application.Common.Extensions;
+global using SSW.Rewards.Application.Common.Interfaces;
 global using SSW.Rewards.Domain.Entities;
 global using SSW.Rewards.Enums;
-global using SSW.Rewards.Application.Common.Interfaces;
-global using AutoMapper;
