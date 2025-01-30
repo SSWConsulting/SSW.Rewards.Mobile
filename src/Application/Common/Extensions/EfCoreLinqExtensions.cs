55f10f8301187f77d8cc2dfe81b3a39dbdecfe9b
Jernej Kavka (JK) [SSW • Microsoft MVP] <JernejKavka@ssw.com.au>
2025-01-30 14:54:38 +1000

    🐛 Fix crashing rewards search and make achievement search return results when query is empty

--- src/Application/Common/Extensions/EfCoreLinqExtensions.cs
+++ src/Application/Common/Extensions/EfCoreLinqExtensions.cs
@@ -1,7 +1,11 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 
 namespace SSW.Rewards.Application.Common.Extensions;
 
+/// <summary>
+/// JK's extension methods for EF Core LINQ queries.
+/// </summary>
 public static class EfCoreLinqExtensions
 {
     /// <summary>
@@ -26,4 +30,49 @@ public static class EfCoreLinqExtensions
         string className = Path.GetFileNameWithoutExtension(callerFileName);
         return className + "-" + callerName + message;
     }
+
+    /// <summary>
+    /// Applies the specified predicate as a filter to the source query only if the given condition is true.
+    /// </summary>
+    /// <typeparam name="TSource">The type of entity being queried.</typeparam>
+    /// <param name="source">The source query.</param>
+    /// <param name="condition">The boolean condition to be checked.</param>
+    /// <param name="predicate">The predicate to be applied as a filter when the condition is true.</param>
+    /// <returns>A new query with the predicate applied as a filter if the condition is true, or the original query otherwise.</returns>
+    public static IQueryable<TSource> WhenTrue<TSource>(this IQueryable<TSource> source, bool condition, Expression<Func<TSource, bool>> predicate)
+        => condition
+        ? source.Where(predicate)
+        : source;
+
+    /// <summary>
+    /// Applies the specified predicate as a filter to the source query only if the provided string value is not empty.
+    /// </summary>
+    /// <typeparam name="TSource">The type of entity being queried.</typeparam>
+    /// <param name="source">The source query.</param>
+    /// <param name="value">The string value to be checked for emptiness.</param>
+    /// <param name="predicate">The predicate to be applied as a filter when the string value is not empty.</param>
+    /// <returns>A new query with the predicate applied as a filter if the string value is not empty, or the original query otherwise.</returns>
+    public static IQueryable<TSource> WhenStringNotEmpty<TSource>(this IQueryable<TSource> source, string? value, Expression<Func<TSource, bool>> predicate)
+        => !string.IsNullOrEmpty(value)
+        ? source.Where(predicate)
+        : source;
+
+    /// <summary>
+    /// Applies the specified predicate as a filter to the source query only if the provided list is not empty.
+    /// </summary>
+    /// <typeparam name="TSource">The type of entity being queried.</typeparam>
+    /// <param name="source">The source query.</param>
+    /// <param name="value">The List to be checked for emptiness.</param>
+    /// <param name="predicate">The predicate to be applied as a filter when the List is not empty.</param>
+    /// <returns>A new query with the predicate applied as a filter if the list is not empty, or the original query otherwise.</returns>
+    public static IQueryable<TSource> WhenNotEmpty<TSource, TList>(this IQueryable<TSource> source, IEnumerable<TList> list, Expression<Func<TSource, bool>> predicate)
+        => list != null && list.Any()
+        ? source.Where(predicate)
+        : source;
+
+    public static IQueryable<TSource> ApplyPagination<TSource>(this IQueryable<TSource> source, IPagedRequest pagedRequest)
+        => ApplyPagination(source, pagedRequest.Page, pagedRequest.PageSize);
+
+    public static IQueryable<TSource> ApplyPagination<TSource>(this IQueryable<TSource> source, int page, int pageSize)
+        => source.Skip(page * pageSize).Take(pageSize);
 }
---
+++ src/Application/Common/Interfaces/IPagedRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace SSW.Rewards.Application.Common.Interfaces;
+
+public interface IPagedRequest
+{
+    public int Page { get; }
+    public int PageSize { get; }
+}
