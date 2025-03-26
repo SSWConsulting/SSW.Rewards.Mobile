dbb73029cdfa0cf316660c8e576cdf157187699e
Zachary Keeping <zacharykeeping@icloud.com>
2025-03-26 16:48:10 +1100

    Set previous tabbar on iPad

--- src/MobileUI/Platforms/iOS/Renderers/CustomShellHandler.cs
+++ src/MobileUI/Platforms/iOS/Renderers/CustomShellHandler.cs
@@ -1,5 +1,6 @@
 using Microsoft.Maui.Controls.Handlers.Compatibility;
 using Microsoft.Maui.Controls.Platform.Compatibility;
+using UIKit;
 
 namespace SSW.Rewards.Mobile.Renderers;
 
@@ -12,4 +13,18 @@ internal class CustomShellHandler : ShellRenderer
             ShellItem = item
         };
     }
+    
+    private sealed class MyShellTabBarAppearanceTracker : ShellTabBarAppearanceTracker
+    {
+        public override void SetAppearance(UITabBarController controller, ShellAppearance appearance)
+        {
+            base.SetAppearance(controller, appearance);
+            if (UIDevice.CurrentDevice.CheckSystemVersion(17, 0))
+            {
+                controller.TraitOverrides.HorizontalSizeClass = UIUserInterfaceSizeClass.Compact;
+
+            }
+        }
+    }
+    protected override IShellTabBarAppearanceTracker CreateTabBarAppearanceTracker() => new MyShellTabBarAppearanceTracker();
 }
