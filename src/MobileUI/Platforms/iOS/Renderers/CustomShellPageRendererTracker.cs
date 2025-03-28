8697299f13856ccf29e338d283a79b5c0cd5a49e
Zachary Keeping <zacharykeeping@icloud.com>
2025-03-28 15:43:11 +1100

    Fix rare crash on iOS

---
+++ src/MobileUI/Platforms/iOS/Renderers/CustomShellPageRendererTracker.cs
@@ -0,0 +1,24 @@
+using Microsoft.Maui.Controls.Platform.Compatibility;
+
+namespace SSW.Rewards.Mobile.Renderers;
+
+public class CustomShellPageRendererTracker : ShellPageRendererTracker
+{
+    public CustomShellPageRendererTracker(IShellContext context)
+        : base(context)
+    {
+
+    }
+
+    protected override void UpdateTabBarVisible()
+    {
+        // Fixes MAUI bug where this can return a NullReferenceException
+        // TODO: Remove when (hopefully) fixed in .NET MAUI 9
+        if (ViewController is null || Page is null)
+        {
+            return;
+        }
+        
+        base.UpdateTabBarVisible();
+    }
+}
