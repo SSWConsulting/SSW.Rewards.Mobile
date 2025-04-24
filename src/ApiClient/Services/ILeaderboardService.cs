30887503fe3882ff968b086736fcd31e78bba8e2
Vlad Kireyev [SSW] <54652701+vladislav-kir@users.noreply.github.com>
2025-04-24 16:18:32 +1000

    Mobile App - Leaderboard incremental load by 50 users (#1254)
    * Mobile App - Leaderboard - incremental load by 50 users
    
    * sort and filter for different tabs on api level
    
    added /Leaderboard/GetPaginated endpoint to use here, since /Leaderboard/Get is used by Admin portal aswell

--- src/ApiClient/Services/ILeaderboardService.cs
+++ src/ApiClient/Services/ILeaderboardService.cs
@@ -7,6 +7,7 @@ namespace SSW.Rewards.ApiClient.Services;
 public interface ILeaderboardService
 {
     Task<LeaderboardViewModel> GetLeaderboard(CancellationToken cancellationToken);
+    Task<LeaderboardViewModel> GetPaginatedLeaderboard(int take, int skip, LeaderboardFilter currentPeriod, CancellationToken cancellationToken);
     Task<LeaderboardViewModel> GetLeaderboard(LeaderboardFilter filter, CancellationToken cancellationToken);
 }
 
@@ -26,6 +27,24 @@ public class LeaderboardService : ILeaderboardService
         var result = await _httpClient.GetAsync($"{_baseRoute}Get", cancellationToken);
 
         if  (result.IsSuccessStatusCode)
+        {
+            var response = await result.Content.ReadFromJsonAsync<LeaderboardViewModel>(cancellationToken: cancellationToken);
+
+            if (response is not null)
+            {
+                return response;
+            }
+        }
+
+        var responseContent = await result.Content.ReadAsStringAsync(cancellationToken);
+        throw new Exception($"Failed to get leaderboard: {responseContent}");
+    }
+
+    public async Task<LeaderboardViewModel> GetPaginatedLeaderboard(int take, int skip, LeaderboardFilter currentPeriod, CancellationToken cancellationToken)
+    {
+        var result = await _httpClient.GetAsync($"{_baseRoute}GetPaginated?skip={skip}&take={take}&currentPeriod={currentPeriod}", cancellationToken);
+
+        if (result.IsSuccessStatusCode)
         {
             var response = await result.Content.ReadFromJsonAsync<LeaderboardViewModel>(cancellationToken: cancellationToken);
 
