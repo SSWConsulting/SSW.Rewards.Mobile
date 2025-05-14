e6f25f24ab4e3da7213901f7956e55f7b447f24a
Zachary Keeping <zacharykeeping@icloud.com>
2025-05-14 13:33:44 +1000

    Add constants

--- src/ApiClient/Constants.cs
+++ src/ApiClient/Constants.cs
@@ -12,6 +12,9 @@ public class Constants
     public const string RewardsQRCodeProtocol = "sswrewards";
     public const string RewardsQRCodeProtocolQueryName = "code";
     public const string RewardsWebDomain = "rewards.ssw.com.au";
+    
+    public const string AppStoreUrl = "https://apps.apple.com/au/app/ssw-rewards/id1634222983";
+    public const string PlayStoreUrl = "https://play.google.com/store/apps/details?id=com.ssw.consulting";
 
     public const string RewardsQRCodeAchievementPrefix = "ach:";
     public const string RewardsQRCodeRewardPrefix = "rwd:";
