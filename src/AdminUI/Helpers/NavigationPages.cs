3619c43a0efe887aac646cbb436e7645e6f43aa1
Jernej Kavka (JK) [SSW • Microsoft MVP] <JernejKavka@ssw.com.au>
2025-05-15 17:24:49 +1000

    Initial Kiosk UI

--- src/AdminUI/Helpers/NavigationPages.cs
+++ src/AdminUI/Helpers/NavigationPages.cs
@@ -2,7 +2,8 @@ using MudBlazor;
 
 namespace SSW.Rewards.Admin.UI.Helpers;
 
-public class NavigationPage {
+public class NavigationPage
+{
     public string Href;
     public string Icon;
     public string Title;
@@ -10,28 +11,27 @@ public class NavigationPage {
 
 public static class NavigationPages
 {
-    public static List<NavigationPage> StaffPages()
-    {
-        return new List<NavigationPage>
-        {
-            new () { Href = "leaderboard", Icon = Icons.Material.Filled.People, Title = "Leaderboard" },
-            new () { Href = "profiles", Icon = Icons.Material.Filled.Person, Title = "Profiles" },
-            new () { Href = "skills", Icon = Icons.Material.Filled.WorkspacePremium, Title = "Skills" },
-        };
-    }
+    public static List<NavigationPage> PublicPages() =>
+        [
+            new() { Href = "kiosk-leaderboard", Icon = Icons.Material.Filled.Leaderboard, Title = "Leaderboard (Kiosk Mode)" }
+        ];
 
-    public static List<NavigationPage> AdminPages()
-    {
-        return new List<NavigationPage>
-        {
-            new () { Href = "achievements", Icon = Icons.Material.Filled.Dashboard, Title = "Achievements" },
-            new () { Href = "rewards", Icon = Icons.Material.Filled.Redeem, Title = "Rewards" },
-            new () { Href = "quizzes", Icon = Icons.Material.Filled.Quiz, Title = "Quizzes" },
-            new () { Href = "users", Icon = Icons.Material.Filled.People, Title = "Users" },
-            new () { Href = "newusers", Icon = Icons.Material.Filled.SupervisedUserCircle, Title = "New Users" },
-            new () { Href = "prizedraw", Icon = Icons.Material.Filled.Celebration, Title = "Prize Draw" },
-            new () { Href = "notifications", Icon = Icons.Material.Filled.Chat, Title = "Notifications" },
-            new () { Href = "delete", Icon = Icons.Material.Filled.Delete, Title = "Delete" },
-        };
-    }
+    public static List<NavigationPage> StaffPages() =>
+        [
+            new() { Href = "leaderboard", Icon = Icons.Material.Filled.People, Title = "Leaderboard" },
+            new() { Href = "profiles", Icon = Icons.Material.Filled.Person, Title = "Profiles" },
+            new() { Href = "skills", Icon = Icons.Material.Filled.WorkspacePremium, Title = "Skills" }
+        ];
+
+    public static List<NavigationPage> AdminPages() =>
+        [
+            new() { Href = "achievements", Icon = Icons.Material.Filled.Dashboard, Title = "Achievements" },
+            new() { Href = "rewards", Icon = Icons.Material.Filled.Redeem, Title = "Rewards" },
+            new() { Href = "quizzes", Icon = Icons.Material.Filled.Quiz, Title = "Quizzes" },
+            new() { Href = "users", Icon = Icons.Material.Filled.People, Title = "Users" },
+            new() { Href = "newusers", Icon = Icons.Material.Filled.SupervisedUserCircle, Title = "New Users" },
+            new() { Href = "prizedraw", Icon = Icons.Material.Filled.Celebration, Title = "Prize Draw" },
+            new() { Href = "notifications", Icon = Icons.Material.Filled.Chat, Title = "Notifications" },
+            new() { Href = "delete", Icon = Icons.Material.Filled.Delete, Title = "Delete" }
+        ];
 }
