0266099fdf4d0d1835678dc10390d988fe1b1f63
Jernej Kavka (JK) [SSW • Microsoft MVP] <JernejKavka@ssw.com.au>
2025-05-23 14:01:59 +1000

    Initial support for sending notification before I needed switch priorities.

---
+++ src/Application/Services/FirebaseInitializerService.cs
@@ -0,0 +1,35 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.Extensions.Options;
+
+namespace SSW.Rewards.Application.Services;
+
+public interface IFirebaseInitializerService
+{
+    void Initialize();
+}
+
+internal class FirebaseInitializerService : IFirebaseInitializerService
+{
+    private readonly Lazy<FirebaseApp> _lazyFirebaseApp;
+
+    public FirebaseInitializerService(IOptions<FirebaseNotificationServiceOptions> options)
+    {
+        _lazyFirebaseApp = new Lazy<FirebaseApp>(() =>
+        {
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                return FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromJson(options.Value.FirebaseCredentials)
+                });
+            }
+            return FirebaseApp.DefaultInstance; // Return the existing instance
+        });
+    }
+
+    public void Initialize()
+    {
+        _ = _lazyFirebaseApp.Value;
+    }
+}
