adc961dc8f8ea56515a98ca60d0517eac7f5a9bd
Jernej Kavka (JK) [SSW] <jernej.kavka@outlook.com>
2025-08-01 15:52:45 +1000

    Add DateTimeFormatter for consistent date formatting in notifications

---
+++ src/AdminUI/Helpers/DateTimeFormatter.cs
@@ -0,0 +1,10 @@
+namespace SSW.Rewards.Admin.UI.Helpers;
+
+public static class DateTimeFormatter
+{
+    public static string FormatLongDate(DateTime? date)
+        => date.HasValue
+            // Format based on https://www.ssw.com.au/rules/weekdays-on-date-selectors/
+            ? $"{date.Value:ddd}, {date.Value:d} {date.Value:t}"
+            : "-";
+}
