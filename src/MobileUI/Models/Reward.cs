e07d0d6e97ad92b579dccb1b8873552e9eec7271
Zach Keeping [SSW] <11418832+zacharykeeping@users.noreply.github.com>
2025-08-06 17:49:52 +1000

    ✨ Mobile | Add offline support to Redeem page (#1389)
    * Enhance offline support for rewards with improved data handling and filtering
    
    * Cleanup
    
    * Improve error handling
    
    * Update src/MobileUI/Services/RewardService.cs
    
    Co-authored-by: Jernej Kavka (JK) [SSW • Microsoft MVP] <jernejk@users.noreply.github.com>
    
    * Update comparison
    
    * Add const
    
    * Bump application display version to 3.1.3
    
    * Refactor reward comparison logic by moving it to `Reward.IsEqual`
    
    ---------
    
    Co-authored-by: Jernej Kavka (JK) [SSW • Microsoft MVP] <jernejk@users.noreply.github.com>

--- src/MobileUI/Models/Reward.cs
+++ src/MobileUI/Models/Reward.cs
@@ -20,5 +20,24 @@ namespace SSW.Rewards.Models
         
         [ObservableProperty]
         private bool _canAfford;
+
+        public static bool IsEqual(Reward? r1, Reward? r2)
+        {
+            if (r1 is null && r2 is null) return true;
+            if (r1 is null || r2 is null) return false;
+            return r1.Id == r2.Id &&
+                   r1.Name == r2.Name &&
+                   r1.Description == r2.Description &&
+                   r1.Cost == r2.Cost &&
+                   r1.IsDigital == r2.IsDigital &&
+                   r1.AwardedAt == r2.AwardedAt &&
+                   r1.Awarded == r2.Awarded &&
+                   r1.ImageUri == r2.ImageUri &&
+                   r1.CarouselImageUri == r2.CarouselImageUri &&
+                   r1.IsCarousel == r2.IsCarousel &&
+                   r1.IsHidden == r2.IsHidden &&
+                   r1.IsPendingRedemption == r2.IsPendingRedemption &&
+                   r1.PendingRedemptionCode == r2.PendingRedemptionCode;
+        }
     }
 }
