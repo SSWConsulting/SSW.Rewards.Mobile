15505b06ab30e7b0d683dd7a68a97b6d56fa84b7
Jernej Kavka (JK) [SSW] <jernej.kavka@outlook.com>
2026-01-05 17:34:24 +1000

    🐛 Removed "," in long date format as per Adam's request
    Moving away from "Mon, 05/01/2025" to "Mon 05/01/2025"

--- src/AdminUI/Helpers/DateTimeFormatter.cs
+++ src/AdminUI/Helpers/DateTimeFormatter.cs
@@ -5,6 +5,6 @@ public static class DateTimeFormatter
     public static string FormatLongDate(DateTime? date)
         => date.HasValue
             // Format based on https://www.ssw.com.au/rules/weekdays-on-date-selectors/
-            ? $"{date.Value:ddd}, {date.Value:d} {date.Value:t}"
+            ? $"{date.Value:ddd} {date.Value:d} {date.Value:t}"
             : "-";
 }
